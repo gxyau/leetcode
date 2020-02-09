92b1590aeb7d478509fb0e8ba547aea2e33808ad
gxyau <GuoXian.Yau@gmail.com>
2020-02-09 14:20:04 +0100

    Combining two sorted link list while preserving sortedness

---
+++ problem21/ListNode.kt
@@ -0,0 +1,15 @@
+package ListNode
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+public class ListNode(var value: Int) {
+    var next: ListNode? = null
+}
