9b7ab564e76445b3bb7fe721063e3db4afcf5912
gxyau <GuoXian.Yau@gmail.com>
2020-03-02 00:01:32 +0100

    Problem 8 solution with regex

---
+++ problem8.kt
@@ -0,0 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.collections.contains as contains1
+
+fun test(str: String) : Int{
+    val pattern = Regex("[\\+\\-]?[1-9]+")
+    val match   = pattern.find(str.trim())?.value
+     
+    return if (match == null) 0 else match.toInt()
+}
+
+
+fun main() {
+    test(str="    4193")
+    test(str="+4")
+    test(str="+h")
+    test(str="    +4-2")
+    test(str="4193 with words")
+}
