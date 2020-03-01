5fe6b894747e3f52a347973489f8ae0ed22a23be
gxyau <gxyau@users.noreply.github.com>
2020-03-02 00:07:17 +0100

    Hotfix regex
    Regex previously search entire string, now only search from the beginning.

--- problem8.kt
+++ problem8.kt
@@ -2,7 +2,7 @@ import java.util.concurrent.atomic.AtomicInteger
 import kotlin.collections.contains as contains1
 
 fun test(str: String) : Int{
-    val pattern = Regex("[\\+\\-]?[1-9]+")
+    val pattern = Regex("^[\\+\\-]?[1-9]+")
     val match   = pattern.find(str.trim())?.value
      
     return if (match == null) 0 else match.toInt()
