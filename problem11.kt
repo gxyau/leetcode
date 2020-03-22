1c4643fa3ba9e8b4ebb112bbebfd94a84beb859f
gxyau <GuoXian.Yau@gmail.com>
2020-03-22 20:25:25 +0100

    Partial solution

--- problem11.kt
+++ problem11.kt
@@ -1,7 +1,22 @@
-fun maxArea(height: IntArray): Int {
+fun maxArea(height: IntArray): Int{
+    val totalElements: Int = height.size
+    var maxArea: Int       = 0
 
+    for(i  in 0 until totalElements - 2){
+        for(j in i until totalElements - 1) {
+            println(listOf(maxArea, (j - i)*minOf(height[i], height[j])))
+            maxArea = maxOf(maxArea, (j - i)*minOf(height[i], height[j]) )
+        }
+    }
+
+    return maxArea
 }
 
 fun main() {
-    maxArea(intArrayOf(0,0))
+    println(maxArea(intArrayOf(0,0)))
+    println(maxArea(intArrayOf(1,2,3,4,5,6,7)))
+    println(maxArea(intArrayOf(7,7,7,7,7)))
+    println(maxArea(intArrayOf(1,2,0,1,3)))
+    println(maxArea(intArrayOf(2,2)))
+    println(maxArea(intArrayOf(7,6,5,4,3,2,1)))
 }
