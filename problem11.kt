c90d9c9df9a730b6d6c86c883eb20f2e76bf1b0a
gxyau <GuoXian.Yau@gmail.com>
2020-03-23 10:24:33 +0100

    Brute force solution

--- problem11.kt
+++ problem11.kt
@@ -2,8 +2,10 @@ fun maxArea(height: IntArray): Int{
     val totalElements: Int = height.size
     var maxArea: Int       = 0
 
-    for(i  in 0 until totalElements - 2){
-        for(j in i until totalElements - 1) {
+    // Only consider outer most bars, and move the shorter one since it is already
+    // the maximum area that it could form
+    for(i  in 0 until totalElements - 1){
+        for(j in i+1 until totalElements) {
             println(listOf(maxArea, (j - i)*minOf(height[i], height[j])))
             maxArea = maxOf(maxArea, (j - i)*minOf(height[i], height[j]) )
         }
@@ -19,4 +21,5 @@ fun main() {
     println(maxArea(intArrayOf(1,2,0,1,3)))
     println(maxArea(intArrayOf(2,2)))
     println(maxArea(intArrayOf(7,6,5,4,3,2,1)))
+    println(maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
 }
