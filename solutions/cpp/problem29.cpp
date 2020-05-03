576415e947e339b5a8f1a0112b7ed5a1f7814e27
gxyau <GuoXian.Yau@gmail.com>
2020-05-03 12:12:30 +0200

    Problem 29, Divide two integers, exceed time limit?

--- solutions/cpp/problem29.cpp
+++ solutions/cpp/problem29.cpp
@@ -2,9 +2,33 @@
 
 class Solution29 {
 public:
+    int signum (int val) {
+        return (int(0) < val) - (int(0) > val);
+    }
+    
     int divide(int dividend, int divisor) {
-        int ans = 0;
+        int ans          = 0;
+        int sign         = signum(dividend) * signum(divisor);
+        long int new_dividend = abs(dividend);
+        long int new_divisor  = abs(divisor);
         
-        return ans;
+        while (true) {
+            if (new_dividend < new_divisor) break;
+            ++ans;
+            new_dividend -= new_divisor;
+        }
+        
+        return sign * ans;
     }
 };
+
+int main() {
+    int dividend, divisor;
+    std::cout << "Enter dividend: " << std::endl;
+    std::cin >> dividend;
+    std::cout << "Enter divisor: " << std::endl;
+    std::cin >> divisor;
+    Solution29 sol;
+    int ans = sol.divide(dividend, divisor);
+    std::cout << "Answer is " << ans << std::endl;
+}
