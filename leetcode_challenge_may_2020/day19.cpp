62ff05e4f17c43b8fe6605db1f39955e6847bae4
gxyau <GuoXian.Yau@gmail.com>
2020-05-19 13:12:15 +0200

    Day 19 Solution, online stock span

--- leetcode_challenge_may_2020/day19.cpp
+++ leetcode_challenge_may_2020/day19.cpp
@@ -3,11 +3,21 @@
 
 class StockSpanner {
 public:
+    stack<int> prices;
+    stack<int> indices;
     StockSpanner() {
-        
+        int count = 0;
     }
     
     int next(int price) {
-        
+        ++count;
+        while (prices.size() != 0 && prices.top() <= price) {
+            count += prices.top();
+            prices.pop();
+            indices.pop();
+        }
+        prices.push(price);
+        indices.push(count);
+        return count;
     }
 };
