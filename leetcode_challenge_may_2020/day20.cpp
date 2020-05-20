28f6ab4d9c4539fb7653cbdf7be110de6baa1a73
gxyau <GuoXian.Yau@gmail.com>
2020-05-20 09:45:57 +0200

    Day 20 initial commit

---
+++ leetcode_challenge_may_2020/day20.cpp
@@ -0,0 +1,20 @@
+// Kth smallest element in a BST
+#include<iostream>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class SolutionDay20 {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        
+    }
+};
