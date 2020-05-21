13ed549e7dab73dddcfb932448239ada938a9beb
gxyau <GuoXian.Yau@gmail.com>
2020-05-21 09:20:12 +0200

    Day 21, initial commit

---
+++ leetcode_challenge_may_2020/day21.cpp
@@ -0,0 +1,9 @@
+// Day 21, Count square submatrices with all ones
+#include<iostream>
+
+class SolutionDay21 {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        return 0;
+    }
+};
