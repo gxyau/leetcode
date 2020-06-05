883bef76377b043deb8ac7c114771a04fe8906ec
gxyau <GuoXian.Yau@gmail.com>
2020-06-05 10:37:26 +0200

    Day 5, initial commit

---
+++ leetcode_challenge_june_2020/day5.cpp
@@ -0,0 +1,20 @@
+// Day 5, Random pick with weight
+#include<iostream>
+#include<vector>
+using std::vector;
+class SolutionDay5 {
+public:
+    Solution(vector<int>& w) {
+        
+    }
+    
+    int pickIndex() {
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
