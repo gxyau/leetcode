9ef28d385c1054ce3306eb49758ae27efb57f7de
gxyau <GuoXian.Yau@gmail.com>
2020-06-06 12:09:52 +0200

    Day 6 initial commit

---
+++ leetcode_challenge_june_2020/day6.cpp
@@ -0,0 +1,11 @@
+// Day 6, Queue reconstruction by height
+#include<iostream>
+#include<vector>
+
+class SolutionDay6 {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        vector<vector<int>> reconstructed_queue;
+        return reconstructed_queue;
+    }
+};
