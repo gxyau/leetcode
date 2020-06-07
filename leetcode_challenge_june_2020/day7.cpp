05e0db5238ecb29fe372aa7839d29a72e152a53d
gxyau <GuoXian.Yau@gmail.com>
2020-06-07 11:05:21 +0200

    Day 7, Added more test cases

--- leetcode_challenge_june_2020/day7.cpp
+++ leetcode_challenge_june_2020/day7.cpp
@@ -14,9 +14,34 @@ public:
 };
 
 int main() {
+    /*
+     * Reminder: An alternative solution to reassigning vectors without pointers
+     * vector<int> v = {1,2,5};
+     * v = {3};
+     * v = {10};
+     * This way we can avoid pointers.
+    */
     SolutionDay7 sol;
-    vector<int> coins1({1,2,5});
-    int comb1 = sol.change(5,coins1);
-    cout << "Number of combinations for this amount: " << comb1 << endl;
+    // Case 1
+    vector<int> *coins = new vector<int>({1,2,5});
+    cout << "Current coins: ";
+    for (int c : *coins) cout << c << " ";
+    cout << endl;
+    int comb = sol.change(5,*coins);
+    cout << "Number of combinations for this amount: " << comb << endl;
+    // Case 2
+    coins = new vector<int>({2});
+    cout << "Current coins: ";
+    for (int c : *coins) cout << c << " ";
+    cout << endl;
+    comb = sol.change(3,*coins);
+    cout << "Number of combinations for this amount: " << comb << endl;
+    // Case 3
+    coins = new vector<int>({10});
+    cout << "Current coins: ";
+    for (int c : *coins) cout << c << " ";
+    cout << endl;
+    comb = sol.change(10,*coins);
+    cout << "Number of combinations for this amount: " << comb << endl;
     return 0;
 }
