7f6ea43f5be42a8417b8b4a46a5a526a44dcbf77
gxyau <GuoXian.Yau@gmail.com>
2020-06-09 13:55:35 +0200

    Problem 38, Count and Say

--- solutions/cpp/problem38.cpp
+++ solutions/cpp/problem38.cpp
@@ -8,8 +8,31 @@ using std::string;
 
 class Solution38 {
 public:
+    string nextString(string s) {
+        string next          = "";
+        char previous_symbol = s[0];
+        int counter = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            char current_symbol = s[i];
+            if (current_symbol == previous_symbol) {
+                ++counter;
+            } else {
+                next += std::to_string(counter) + previous_symbol;
+                counter = 1;
+                previous_symbol = current_symbol;
+            }
+        }
+        // Need to append last symbol to string
+        next += std::to_string(counter) + previous_symbol;
+        return next;
+    }
     string countAndSay(int n) {
-        string say = "Something";
+        string say = "1";
+        int index  = 1;
+        while (index < n) {
+            say = nextString(say);
+            ++index;
+        }
         return say;
     }
 };
@@ -17,10 +40,8 @@ public:
 int main() {
     Solution38 sol;
     int    k;
-    string ans;
     cout << "Please enter a number, k: ";
     cin  >> k;
-    ans = countAndSay(k);
-    cout << "The kth term of count and say sequence is: " << ans << endl;
+    cout << "The kth term of count and say sequence is: " << sol.countAndSay(k) << endl;
     return 0;
 }
