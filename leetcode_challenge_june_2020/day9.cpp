61250ded97fe9abed5e12b0ef5539151e96390c0
gxyau <GuoXian.Yau@gmail.com>
2020-06-09 11:19:21 +0200

    Day 9, Is subsequence, one pass solution

--- leetcode_challenge_june_2020/day9.cpp
+++ leetcode_challenge_june_2020/day9.cpp
@@ -9,17 +9,23 @@ using std::string;
 class SolutionDay9 {
 public:
     bool isSubsequence(string s, string t) {
-        return true;
+        int matched = 0;
+        string::iterator it = s.begin();
+        for (int i = 0; i < t.size(); ++i) {
+            if (t[i] == *it) {++matched; ++it;}
+        }
+        return matched == s.size();
     }
 };
 
 int main() {
-    string substring, target_string;
+    string       substring, target_string;
+    SolutionDay9 sol;
     cout << "Insert substring: ";
     cin  >> substring;
     cout << "Insert target string: ";
     cin  >> target_string;
     cout << substring << " is a substring of " << target_string
-         << ": " << isSubsequence(substring, target_string) << endl;
+         << ": " << sol.isSubsequence(substring, target_string) << endl;
     return 0;
 }
