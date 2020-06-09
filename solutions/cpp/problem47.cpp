a4afe8503ade677b79beec7ced1157a64a235518
gxyau <GuoXian.Yau@gmail.com>
2020-06-09 15:48:56 +0200

    Problem 47, Permutations II, Using problem 31 next permutation

--- solutions/cpp/problem47.cpp
+++ solutions/cpp/problem47.cpp
@@ -1,10 +1,19 @@
 // Problem 47, Permutations II
+#include<problem31>
 #include<vector>
 using std::vector;
 
 class Solution47 {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
+        Solution31 sol;
+        vector<vector<int>> permutations;
+        vector<int> initial = nums;
+        while (true) {
+            permutations.push_back(nums);
+            sol.nextPermutation(nums);
+            if (nums == initial) break;
+        }
+        return permutations;
     }
 };
