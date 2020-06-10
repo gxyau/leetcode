c50881db16810328ee1b60bb8f65b25f7466b1a0
gxyau <GuoXian.Yau@gmail.com>
2020-06-10 16:01:34 +0200

    Problem 216, Basic structure

--- solutions/cpp/problem216.cpp
+++ solutions/cpp/problem216.cpp
@@ -1,6 +1,10 @@
 // Combination Sum III
 #include<iostream>
 #include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
 
 class Solution216 {
 public:
@@ -9,3 +13,21 @@ public:
         return combinations;
     }
 };
+
+int main() {
+    int n,k;
+    Solution216 sol;
+    cout << "Please insert a positive integer, k: ";
+    cin  >> k;
+    cout << "Please insert a positive integer, n: ";
+    cin  >> n;
+    vector<vector<int>> valid_combinations = sol.combinationSum3(k,n);
+    cout << "The valid combinations are: ";
+    for (auto &vec : valid_combinations) {
+        for (vector<int>::iterator it = vec.begin(); it != vec.end(); ++it){
+            cout << *it << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
