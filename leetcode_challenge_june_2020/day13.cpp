0cc438c1fad4ee72476e44a0f6462bede5c5ff8e
gxyau <GuoXian.Yau@gmail.com>
2020-06-13 12:08:00 +0200

    Day 13, Largest Divisible Set, DP

---
+++ leetcode_challenge_june_2020/day13.cpp
@@ -0,0 +1,93 @@
+// Day 13, Largest Divisible Set
+#include<algorithm>
+#include<iostream>
+#include<utility>
+#include<vector>
+using std::cout;
+using std::endl;
+using std::pair;
+using std::vector;
+
+class SolutionDay13 {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        // Sorting the vector
+        int n = nums.size();
+        if(n<=1) return nums;
+        std::sort(nums.begin(), nums.end());
+        
+        // pair.first contains length longest subsequence,
+        // pair.second contains its parent index
+        vector<pair<int, int>> DP(n, pair<int, int> (0,-1));
+        
+        /*
+         * We check the largest number before nums[i] that is divisible by it
+         */
+        for (int i = 0; i < n; ++i) {
+            int j = i;
+            while (--j >= 0) {
+                if (nums[i] % nums[j] == 0
+                        && DP[i].first <= DP[j].first) {
+                    DP[i].first = DP[j].first + 1;
+                    DP[i].second = j;
+                }
+            }
+        }
+        
+        // Return answer
+        vector<int> answer;
+        int index = n-1;
+        while (true) {
+            answer.push_back(nums[index]);
+            index = DP[index].second;
+            if (index < 0) break;
+        }
+        std::reverse(answer.begin(), answer.end());
+        return answer;
+    }
+};
+
+int main() {
+    SolutionDay13 sol;
+    // Case 1
+    vector<int> numbers = {1,2,3};
+    vector<int> largest = sol.largestDivisibleSubset(numbers);
+    cout << "Case1, the largest subset is: " << endl;
+    for (int x : largest) {
+        cout << x << " ";
+    }
+    cout << endl;
+    // Case 2
+    numbers = {2,3,4};
+    largest = sol.largestDivisibleSubset(numbers);
+    cout << "Case 2, the largest subset is: " << endl;
+    for (int x : largest) {
+        cout << x << " ";
+    }
+    cout << endl;
+    // Case 3
+    numbers = {1,2,3,4,5,6,8};
+    largest = sol.largestDivisibleSubset(numbers);
+    cout << "Case 3, the largest subset is: " << endl;
+    for (int x : largest) {
+        cout << x << " ";
+    }
+    cout << endl;
+    // Case 4
+    numbers = {1,2,4,8};
+    largest = sol.largestDivisibleSubset(numbers);
+    cout << "Case 4, the largest subset is: " << endl;
+    for (int x : largest) {
+        cout << x << " ";
+    }
+    // Case 5
+    cout << endl;
+    numbers = {2,4,1,8};
+    largest = sol.largestDivisibleSubset(numbers);
+    cout << "Case 4, the largest subset is: " << endl;
+    for (int x : largest) {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
