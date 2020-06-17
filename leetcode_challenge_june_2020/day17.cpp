e3ffd6ea62d1e0ea0fb6388ddfccff32a6fee011
gxyau <GuoXian.Yau@gmail.com>
2020-06-17 11:21:04 +0200

    Day 17, Initial commit

---
+++ leetcode_challenge_june_2020/day17.cpp
@@ -0,0 +1,13 @@
+// Day 17
+#include<iostream>
+#include<vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+class SolutionDay17 {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+    }
+};
