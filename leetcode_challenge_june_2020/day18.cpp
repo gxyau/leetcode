66a1d1bbe0b85d12254c77a556f778669d212986
gxyau <GuoXian.Yau@gmail.com>
2020-06-18 11:15:09 +0200

    Day 18, initial commit

---
+++ leetcode_challenge_june_2020/day18.cpp
@@ -0,0 +1,17 @@
+// Day 18, H-Index II
+#include<iostream>
+#include<vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+    }
+};
+
+int main() {
+    return 0;
+}
