ec820de16ea6b6317e50866f158a76ce6bc8a049
gxyau <GuoXian.Yau@gmail.com>
2020-06-18 11:44:39 +0200

    Day 18, Skeleton of the solution

--- leetcode_challenge_june_2020/day18.cpp
+++ leetcode_challenge_june_2020/day18.cpp
@@ -5,13 +5,29 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-class Solution {
+class SolutionDay18 {
 public:
     int hIndex(vector<int>& citations) {
-        
+        return 0;
     }
 };
 
 int main() {
+    SolutionDay18 sol;
+    vector<int> citations;
+    // Case 1
+    citations = {0,1,3,5,8};
+    for (int x : citations) {
+        cout << x << " ";
+    }
+    cout << endl;
+    cout << "The H-Index for this author is: " << sol.hIndex(citations) << endl;
+    // Case 2
+    citations = {0,1,4,5,6};
+    for (int x : citations) {
+        cout << x << " ";
+    }
+    cout << endl;
+    cout << "The H-Index for this author is: " << sol.hIndex(citations) << endl;
     return 0;
 }
