058f3e545ebb8895e6207d8a47e02d22783cc8d9
gxyau <GuoXian.Yau@gmail.com>
2020-06-19 15:04:06 +0200

    Day 19, Longest Duplicate String

--- leetcode_challenge_june_2020/day19.cpp
+++ leetcode_challenge_june_2020/day19.cpp
@@ -9,8 +9,34 @@ using std::string;
 class SolutionDay19 {
 public:
     string longestDupSubstring(string S) {
-        string substring = "";
-        return substring;
+        string longest;
+        std::unordered_set<std::string_view> set;
+        size_t beg = 1;
+        size_t end = S.size() - 1;
+        while (beg <= end)
+        {
+            auto len = beg + (end - beg) / 2;
+            bool found = false;
+            for (size_t i = 0; i != S.size() - len + 1; ++i)
+            {
+                const auto [it, inserted] = set.emplace(S.data() + i, len);
+                if (!inserted)
+                {
+                    found = true;
+                    longest = std::move(*it);
+                    break;   
+                }
+            }
+            
+            if (found)
+                beg = len + 1;
+            else
+                end = len - 1;
+            
+            set.clear();      
+        }
+            
+        return longest;
     }
 };
 
