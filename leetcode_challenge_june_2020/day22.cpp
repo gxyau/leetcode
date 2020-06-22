4b58cc93882ac6fea93c19e541b96d291c8e6402
gxyau <GuoXian.Yau@gmail.com>
2020-06-22 10:50:17 +0200

    Day 22, Skeleton

---
+++ leetcode_challenge_june_2020/day22.cpp
@@ -0,0 +1,17 @@
+// Day 22, Single Number II
+#include<iostream>
+#include<vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+class SolutionDay22 {
+public:
+    int singleNumber(vector<int>& nums) {
+        return 0;
+    }
+};
+
+int main() {
+    return 0;
+}
