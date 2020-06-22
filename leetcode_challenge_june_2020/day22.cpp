3d63acf656db1b1966d316ef3880349b6b3fcaa6
gxyau <GuoXian.Yau@gmail.com>
2020-06-22 10:59:04 +0200

    Day 22, Single Number II

--- leetcode_challenge_june_2020/day22.cpp
+++ leetcode_challenge_june_2020/day22.cpp
@@ -8,7 +8,13 @@ using std::vector;
 class SolutionDay22 {
 public:
     int singleNumber(vector<int>& nums) {
-        return 0;
+        // 3 * unique sum is 2 * target more than total sum
+        int sum = 0, unique_sum = 0;
+        for (int x : nums) {
+            unique_sum ^= x;
+            sum        += x;
+        }
+        return (int) (3*unique_sum - sum)/2;
     }
 };
 
@@ -17,7 +23,24 @@ int main() {
     vector<int> nums;
     int answer;
     // Case 1
-    nums   = new vector<int>({0,0,0,1});
+    nums   = {0,0,0,1};
+    cout << "The vector is: ";
+    for (int x : nums) cout << x << " ";
+    cout << endl;
+    answer = sol.singleNumber(nums);
+    cout << "The answer is: " << answer << endl;
+    // Case 2
+    nums   = {1,2,0,1,0,0,1};
+    cout << "The vector is: ";
+    for (int x : nums) cout << x << " ";
+    cout << endl;
+    answer = sol.singleNumber(nums);
+    cout << "The answer is: " << answer << endl;
+    // Case 3
+    nums   = {1};
+    cout << "The vector is: ";
+    for (int x : nums) cout << x << " ";
+    cout << endl;
     answer = sol.singleNumber(nums);
     cout << "The answer is: " << answer << endl;
     return 0;
