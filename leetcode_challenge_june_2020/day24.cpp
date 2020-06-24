c411a38d86deb6e18dfa1dd0ef792ccea27e84b0
gxyau <GuoXian.Yau@gmail.com>
2020-06-24 12:07:38 +0200

    Day 24, Unique BST, Catalan without overflow problem

--- leetcode_challenge_june_2020/day24.cpp
+++ leetcode_challenge_june_2020/day24.cpp
@@ -1,21 +1,26 @@
 // Day 24, Unique Binary Search Tree
+#include <iostream>
 
 class SolutionDay24 {
+private:
+    double round(double d) {
+      return floor(d + 0.5);
+    }
 public:
     int numTrees(int n) {
-        // Apparently solution is Catalan number, should have thought so
-        int catalan = 1, k = n;
-        while(k>1) {
-            catalan *= (n+k--);
+        double catalan = 1, m = n, k = n;
+        while (k>1) {
+            catalan *= (m+k)/k;
+            --k;
         }
-        return catalan;
+        return (int) round(catalan);
     }
 };
 
 int main() {
     SolutionDay24 sol;
     int catalan, n;
-    std::cout << "Please enter a positive integer" << endl;
+    std::cout << "Please enter a positive integer: ";
     std::cin  >> n;
     catalan = sol.numTrees(n);
     std::cout << "Number of unique BST on " << n << " vertices is: " << catalan << std::endl;
