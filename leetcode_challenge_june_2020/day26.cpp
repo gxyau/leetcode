d9ba9da9de12779fab91a7e62e8c599bf8d132d2
gxyau <GuoXian.Yau@gmail.com>
2020-06-26 10:21:04 +0200

    Day 26, Initial commit

---
+++ leetcode_challenge_june_2020/day26.cpp
@@ -0,0 +1,19 @@
+// Day 26, Sum Root to Leaf Numbers
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class SolutionDay26 {
+public:
+    int sumNumbers(TreeNode* root) {
+        
+    }
+};
