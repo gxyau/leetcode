a8e52df39bf710b2c0bc4c60e4c01200603cfed0
gxyau <GuoXian.Yau@gmail.com>
2020-06-28 12:13:01 +0200

    Day 28, Skeleton

---
+++ leetcode_challenge_june_2020/day28.cpp
@@ -0,0 +1,20 @@
+// Day 28, Reconstruct Itinerary
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+class SolutionDay28 {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        vector<string> itinerary;
+        return itenerary;
+    }
+};
+
+int main() {
+    return 0;
+}
