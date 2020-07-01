8c9b0de044e58b042623657a091d86abfd112a5d
gxyau <GuoXian.Yau@gmail.com>
2020-07-01 09:16:04 +0200

    Renaming directories, previous name too long

--- leetcode_challenge_april_2020/src/day19.kt
+++
@@ -1,9 +0,0 @@
-class SolutionDay19 {
-    fun search(nums: IntArray, target: Int): Int {
-        for (index in nums.indices) {
-            if (target == nums[index]) return index
-        }
-
-        return -1
-    }
-}
