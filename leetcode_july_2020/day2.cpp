52fbc2e74a0d5b0a21555b739de3906cc011507e
gxyau <GuoXian.Yau@gmail.com>
2020-07-02 14:14:02 +0200

    Day 2, Binary Tree Level Order Traversal II, queue

--- leetcode_july_2020/day2.cpp
+++ leetcode_july_2020/day2.cpp
@@ -1,4 +1,5 @@
 // Day 2, Binary Tree Level Order Traversal II
+#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -18,8 +19,30 @@ struct TreeNode {
 class SolutionDay2 {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if (root == nullptr) return vector<vector<int>> (0);
+        vector<vector<int>> reversed;
+        vector<int> level;
+        queue<TreeNode*> q1, q2;
+        q1.push(root);
+        TreeNode* ptr;
+        while (!q1.empty() || !q2.empty()) {
+            if (!q1.empty()) {
+                ptr = q1.front();
+                q1.pop();
+                level.push_back(ptr->val);
+                if (ptr->left) q2.push(ptr->left);
+                if (ptr->right) q2.push(ptr->right);
+            } else { // q2 not empty
+                q1 = q2;
+                q2 = queue<TreeNode*>();
+                reversed.push_back(level);
+                level.resize(0);
+            }
+        }
+        reversed.push_back(level);
         // Two queues, level traversal
-        return vector<vector<int>>(1,vector<int>(1));
+        std::reverse(reversed.begin(), reversed.end());
+        return reversed;
     }
 };
 
@@ -34,7 +57,7 @@ int main() {
     root->right->right = new TreeNode(7);
     vector<vector<int>> reversed = sol.levelOrderBottom(root);
     // Printing levels
-    std::cout << "The levels are: ";
+    std::cout << "The levels are: \n";
     for (auto vec : reversed) {
         for (auto x : vec) {
             std::cout << x << "  ";
