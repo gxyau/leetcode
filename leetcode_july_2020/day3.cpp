a77ffec428109e91dd07e654b1b11a8ed46def43
gxyau <GuoXian.Yau@gmail.com>
2020-07-03 09:53:14 +0200

    Day 3 skeleton

---
+++ leetcode_july_2020/day3.cpp
@@ -0,0 +1,21 @@
+// Day 3, Prison Cell After N Days
+#include<iostream>
+#include<vector>
+using std::vector;
+
+class SolutionDay3 {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        vector<int> new_cell;
+        
+        return new_cell;
+    }
+};
+
+int main() {
+    SolutionDay3 sol;
+    int N;
+    // Test case 1
+    // Compiled successfully
+    return 0;
+}
