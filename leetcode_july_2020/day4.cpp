b10ae12aad588c3b21735b84aa9141301df4718f
gxyau <GuoXian.Yau@gmail.com>
2020-07-04 11:48:59 +0200

    Day 4, Initial solution

---
+++ leetcode_july_2020/day4.cpp
@@ -0,0 +1,29 @@
+// Day 4, Ugly Number II
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class SolutionDay4 {
+public:
+    int nthUglyNumber(int n) {
+        int n1, n2, n3, nth_ugliest = 1;
+        while (--n) {
+            n1          = nth_ugliest * 2;
+            n2          = nth_ugliest * 3;
+            n3          = nth_ugliest * 5;
+            nth_ugliest = std::min({n1,n2,n3});
+        }
+        return nth_ugliest;
+    }
+};
+
+int main() {
+    SolutionDay4 sol;
+    int n, ans;
+    std::cout << "Please enter a positive integer, n: ";
+    std::cin  >> n;
+    ans = sol.nthUglyNumber(n);
+    std::cout << "The nth ugliest number is: " << ans << std::endl;
+    // Compile successful
+    return 0;
+}
