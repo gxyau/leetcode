a727b43f2f066bf18a8256a0fc7ed4451f622cd6
gxyau <GuoXian.Yau@gmail.com>
2020-07-05 12:53:35 +0200

    Problem 477, Skeleton

---
+++ solutions/cpp/problem477.cpp
@@ -0,0 +1,16 @@
+// Problem 477, Total Hamming Distance
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        return 0;
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
