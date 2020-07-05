8dfc888dce088c4db18e2d618d9c763f14a489b6
gxyau <GuoXian.Yau@gmail.com>
2020-07-05 11:01:29 +0200

    Day 4, Solution

--- leetcode_july_2020/day4.cpp
+++ leetcode_july_2020/day4.cpp
@@ -1,17 +1,23 @@
-// Day 4, Ugly Number II
+// Day 4, Ugly Number II'
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 class SolutionDay4 {
 public:
     int nthUglyNumber(int n) {
-        int n1, n2, n3, nth_ugliest = 1;
-        while (--n) {
-            n1          = nth_ugliest * 2;
-            n2          = nth_ugliest * 3;
-            n3          = nth_ugliest * 5;
-            nth_ugliest = n1 < n2 ? std::min(n1, n3) : std::min(n2,n3);
+        int n1 = 0, n2 = 0, n3 = 0;
+        std::vector<int> DP;
+        DP.push_back(1);
+        int i=1;
+        while(i<n){
+            DP.push_back(std::min({2*DP[n1],3*DP[n2],5*DP[n3]}));
+            if(DP[i]==DP[n1]*2) n1++;
+            if(DP[i]==DP[n2]*3) n2++;
+            if(DP[i]==DP[n3]*5) n3++;
+            i++;
         }
-        return nth_ugliest;
+        return DP[n-1];
     }
 };
 
