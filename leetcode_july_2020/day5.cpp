d0ede3669e878ca34159322330354d0be3ea8fdd
gxyau <GuoXian.Yau@gmail.com>
2020-07-05 11:02:57 +0200

    Day 5, Skeleton

---
+++ leetcode_july_2020/day5.cpp
@@ -0,0 +1,13 @@
+// Day 5, Hamming distance
+class SolutionDay5 {
+public:
+    int hammingDistance(int x, int y) {
+        int hamming = 0;
+        return hamming;
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
