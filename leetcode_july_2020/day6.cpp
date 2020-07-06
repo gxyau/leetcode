1475b7edc506c5a7a982249910f94f1c4ef029b5
gxyau <GuoXian.Yau@gmail.com>
2020-07-06 10:06:33 +0200

    Day 6, Plus one, Very slow solution

--- leetcode_july_2020/day6.cpp
+++ leetcode_july_2020/day6.cpp
@@ -1,16 +1,57 @@
 // Day 6, Plus One
+#include <algorithm>
+#include <iostream>
 #include <vector>
 using std::vector;
 
-class Solution {
+class SolutionDay6 {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        ++digits[digits.size()-1];
-        return digits;
+        vector<int> ans;
+        int carry = 1, n = digits.size();
+        // Add 1 then build new array
+        for (int i = 0; i < n; ++i) {
+            if (carry) ++digits[n-1-i];
+            carry = (digits[n-1-i] / 10) > 0;
+            digits[n-1-i] %= 10;
+            ans.push_back(digits[n-1-i]);
+        }
+        if (carry) ans.push_back(1);
+        std::reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 
 int main() {
+    SolutionDay6 sol;
+    vector<int> digits, ans;
+    // Test case 1
+    digits = {-1,2};
+    std::cout << "Current number: ";
+    for (int x : digits) std::cout << x;
+    std::cout << "\n";
+    ans    = sol.plusOne(digits);
+    std::cout << "New number: ";
+    for (int x : ans) std::cout << x;
+    std::cout << "\n\n";
+    // Test case 1
+    digits = {9};
+    std::cout << "Current number: ";
+    for (int x : digits) std::cout << x;
+    std::cout << "\n";
+    ans    = sol.plusOne(digits);
+    std::cout << "New number: ";
+    for (int x : ans) std::cout << x;
+    std::cout << "\n\n";
+    // Test case 1
+    digits = {-1,9};
+    std::cout << "Current number: ";
+    for (int x : digits) std::cout << x;
+    std::cout << "\n";
+    ans    = sol.plusOne(digits);
+    std::cout << "New number: ";
+    for (int x : ans) std::cout << x;
+    std::cout << "\n\n";
     // Compile successful
     return 0;
 }
