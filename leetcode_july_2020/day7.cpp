926b48c96cb139f20ed051cb26b8557e9a701f29
gxyau <GuoXian.Yau@gmail.com>
2020-07-07 12:39:30 +0200

    Day 7, skeleton

---
+++ leetcode_july_2020/day7.cpp
@@ -0,0 +1,17 @@
+// Day 7, Island Perimeter
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+        return perimeter;
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
