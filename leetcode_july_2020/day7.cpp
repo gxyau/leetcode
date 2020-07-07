63984794bedf8fb42fa2afb3b1fab4ef3ba92ba1
gxyau <GuoXian.Yau@gmail.com>
2020-07-07 12:45:07 +0200

    Added test cases

--- leetcode_july_2020/day7.cpp
+++ leetcode_july_2020/day7.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using std::vector;
 
-class Solution {
+class SolutionDay7 {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int perimeter = 0;
@@ -12,6 +12,64 @@ public:
 };
 
 int main() {
+    SolutionDay7 sol;
+    vector<vector<int>> grid;
+    int perimeter;
+    // Test case 1
+    grid      = {{0}};
+    std::cout << "The grid is:\n";
+    for (auto vec : grid) {
+        for (int x : vec) {
+            std::cout << x << "  ";
+        }
+        std::cout << "\n";
+    }
+    perimeter = sol.islandPerimeter(grid);
+    std::cout << "The perimeter is: " << perimeter << "\n\n";
+    // Test case 2
+    grid      = {{1}};
+    std::cout << "The grid is:\n";
+    for (auto vec : grid) {
+        for (int x : vec) {
+            std::cout << x << "  ";
+        }
+        std::cout << "\n";
+    }
+    perimeter = sol.islandPerimeter(grid);
+    std::cout << "The perimeter is: " << perimeter << "\n\n";
+    // Test case 3
+    grid      = {{1,0}};
+    std::cout << "The grid is:\n";
+    for (auto vec : grid) {
+        for (int x : vec) {
+            std::cout << x << "  ";
+        }
+        std::cout << "\n";
+    }
+    perimeter = sol.islandPerimeter(grid);
+    std::cout << "The perimeter is: " << perimeter << "\n\n";
+    // Test case 4
+    grid      = {{1,1}};
+    std::cout << "The grid is:\n";
+    for (auto vec : grid) {
+        for (int x : vec) {
+            std::cout << x << "  ";
+        }
+        std::cout << "\n";
+    }
+    perimeter = sol.islandPerimeter(grid);
+    std::cout << "The perimeter is: " << perimeter << "\n\n";
+    // Test case 5
+    grid      = {{0,1,0,0}, {1,1,1,0},{0,1,0,0},{1,1,0,0}};
+    std::cout << "The grid is:\n";
+    for (auto vec : grid) {
+        for (int x : vec) {
+            std::cout << x << "  ";
+        }
+        std::cout << "\n";
+    }
+    perimeter = sol.islandPerimeter(grid);
+    std::cout << "The perimeter is: " << perimeter << "\n\n";
     // Compile successful
     return 0;
 }
