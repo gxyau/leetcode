41c2df17c75893b19e9f4dde1f32268c5252dd3b
gxyau <GuoXian.Yau@gmail.com>
2020-07-08 10:28:18 +0200

    Day 8, Initial solution, need test cases

---
+++ leetcode_july_2020/day8.cpp
@@ -0,0 +1,34 @@
+// DAy 8, 3 Sum
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int n = nums.size(), i = 0;
+        while (i < n-2) {
+            int l = i+1, r = n-1;
+            while(l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum<0) {
+                    ++l;
+                } else if(sum > 0) {
+                    --r;
+                } else {
+                    temp = {nums[i], nums[l], nums[r]};
+                    ans.push_back(temp);
+                    while(l < r && nums[l] == temp[1]) ++l;
+                }
+            }
+            do {
+                ++i;
+            } while (nums[i]==nums[i+1]);
+        }
+        return ans;
+    }
+};
