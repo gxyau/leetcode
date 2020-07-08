eab7e07a6e9394b139261539715d3adb6ce6f282
gxyau <GuoXian.Yau@gmail.com>
2020-07-08 10:55:11 +0200

    Day 8, 3 sum

--- leetcode_july_2020/day8.cpp
+++ leetcode_july_2020/day8.cpp
@@ -1,4 +1,4 @@
-// DAy 8, 3 Sum
+// Day 8, 3 Sum
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -10,12 +10,12 @@ public:
         std::sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         vector<int> temp;
-        int n = nums.size(), i = 0;
-        while (i < n-2) {
+        int n = nums.size();
+        for (int i = 0; i < n-2; ++i) {
             int l = i+1, r = n-1;
             while(l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
-                if(sum<0) {
+                if(sum < 0) {
                     ++l;
                 } else if(sum > 0) {
                     --r;
@@ -25,9 +25,7 @@ public:
                     while(l < r && nums[l] == temp[1]) ++l;
                 }
             }
-            do {
-                ++i;
-            } while (nums[i]==nums[i+1]);
+            while (i < n-2 && nums[i]==nums[i+1]) ++i; // We want the first one not the last one
         }
         return ans;
     }
