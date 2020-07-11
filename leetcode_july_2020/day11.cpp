867cf607ec3157992ef92430aabb864578e5a863
gxyau <GuoXian.Yau@gmail.com>
2020-07-11 10:59:29 +0200

    Day 11, Subsets. skeleton

---
+++ leetcode_july_2020/day11.cpp
@@ -0,0 +1,18 @@
+// Day 11, Subsets
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class SolutionDay11 {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> powerset;
+        int n = nums.size();
+        return powerset;
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
