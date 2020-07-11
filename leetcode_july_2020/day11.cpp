d0b77c302b2f4cba2f4064a554bafc4fe95c373e
gxyau <GuoXian.Yau@gmail.com>
2020-07-11 12:15:51 +0200

    Day 11, Subset (Powerset), Bit manipulation

--- leetcode_july_2020/day11.cpp
+++ leetcode_july_2020/day11.cpp
@@ -1,13 +1,28 @@
 // Day 11, Subsets
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using std::vector;
 
 class SolutionDay11 {
+private:
+    vector<int> compute(int k, vector<int>& nums) {
+        vector<int> subset;
+        int n = nums.size(), num;
+        while (n--) {
+            if (k & (1 << n)) subset.push_back(nums[n]);
+        }
+        std::reverse(subset.begin(), subset.end());
+        return subset;
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> powerset;
-        int n = nums.size();
+        int k = 1 << nums.size();
+        while (k--) {
+            powerset.push_back(compute(k, nums));
+        }
+        std::reverse(powerset.begin(), powerset.end());
         return powerset;
     }
 };
