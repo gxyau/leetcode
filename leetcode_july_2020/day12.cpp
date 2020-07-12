2dadf83a5c148300bb6d1ba20acde721c245ffe6
gxyau <GuoXian.Yau@gmail.com>
2020-07-12 10:38:10 +0200

    Day 12, Reverse Bits, Bit operations

--- leetcode_july_2020/day12.cpp
+++ leetcode_july_2020/day12.cpp
@@ -4,16 +4,23 @@
 class SolutionDay12 {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t bits = 0, tmp;
+        uint32_t bits = 0, tmp, iteration = 32;
+        while (iteration--) {
+            tmp  = (n & 1);
+            bits = (bits << 1) | tmp;
+            n  >>= 1;
+        }
         return bits;
     }
 };
 
 int main() {
-    uint32_t n;
-    std::cout << "Please enter a number: " << std::endl;
+    SolutionDay12 sol;
+    uint32_t n, rev;
+    std::cout << "Please enter a number: ";
     std::cin  >> n;
-    std::cout << n << std::endl;
+    rev = sol.reverseBits(n);
+    std::cout << rev << std::endl;
     // Compile successful
     return 0;
 }
