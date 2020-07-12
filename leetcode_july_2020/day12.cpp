1ffbf65b9e30f8c1f31597b601398ce9305ab452
gxyau <GuoXian.Yau@gmail.com>
2020-07-12 10:30:03 +0200

    Day 12, skeleton

---
+++ leetcode_july_2020/day12.cpp
@@ -0,0 +1,19 @@
+// Day 12, Reverse Bits
+#include <iostream>
+
+class SolutionDay12 {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t bits = 0, tmp;
+        return bits;
+    }
+};
+
+int main() {
+    uint32_t n;
+    std::cout << "Please enter a number: " << std::endl;
+    std::cin  >> n;
+    std::cout << n << std::endl;
+    // Compile successful
+    return 0;
+}
