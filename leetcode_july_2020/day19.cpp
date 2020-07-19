ef368751567836f2cb2eede955c712e808de2960
gxyau <GuoXian.Yau@gmail.com>
2020-07-19 15:15:55 +0200

    Day 19, Add binary, Bit manipulation and string int conversion

--- leetcode_july_2020/day19.cpp
+++ leetcode_july_2020/day19.cpp
@@ -1,4 +1,5 @@
 // Day 19, Add Binary
+#include <algorithm>
 #include <bitset>
 #include <iostream>
 #include <string>
@@ -7,6 +8,7 @@ using std::string;
 class SolutionDay19 {
 public:
     string addBinary(string a, string b) {
+        /*
         unsigned long int sum = 0, num1 = std::stoi(a, nullptr, 2), num2 = std::stoi(b, nullptr, 2);
         int bit1, bit2, carry = 0, pos = 0, n = std::max(a.size(), b.size());
         while (n--) {
@@ -17,8 +19,22 @@ public:
         }
         if (carry) sum += 1 << pos++;
         string ans = std::bitset<64>(sum).to_string();
+        */
+        if (a.size() < b.size()) std::swap(a,b);
+        string result="";
+        int m = a.length(), n = b.length();
+        int carry = 0, sum = 0, bit1, bit2;
+        while(m > 0 || n > 0) {
+            bit1    = a[--m] - '0';
+            bit2    = n > 0 ? b[--n] - '0' : 0; // Only happens if unequal length
+            sum     = bit1 + bit2 + carry;
+            result += std::to_string(sum & 1);    // integer to string
+            carry   = sum >> 1;
+        }
+        if (carry) result += '1';
+        std::reverse(result.begin(), result.end());
         // Return answer
-        return ans.substr(ans.size()-pos);
+        return result;
     }
 };
 
