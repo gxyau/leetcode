da870f6a778c3114605ba361885133973f211653
gxyau <GuoXian.Yau@gmail.com>
2020-07-21 10:55:22 +0200

    Partial solution, DFS

--- leetcode_july_2020/day21.cpp
+++ leetcode_july_2020/day21.cpp
@@ -1,12 +1,30 @@
 // Day 21, Word Search
 #include <iostream>
+#include <string>
 #include <vector>
+using std::string;
 using std::vector;
 
 class SolutionDay21 {
+private:
+    bool search(int i, int j, vector<vector<char>>& board, string remaining) {
+        if (remaining == '') return true;
+        int m = board.size(), n = board[0].size();
+        if (board[i][j] == '0') {
+            return false;
+        } else if (board[i][j] == string[0]) {
+            char tmp = string[0];
+            board[i][j] = '0';
+            if (i > 0)   return search(i-1,j,board, string.substr(1));
+            if (j > 0)   return search(i,j-1,board, string.substr(1));
+            if (i < n-1) return search(i+1,j,board, string.substr(1));
+            if (j < n-1) return search(i,j+1,board, string.substr(1));
+            board[i][j] = tmp;
+        }
+    }
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        return true;
+        return false;
     }
 };
 
