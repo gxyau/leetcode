a88d3c66a0f0c31b18a39ddca32f7774994b2cc4
gxyau <GuoXian.Yau@gmail.com>
2020-07-21 10:41:39 +0200

    Day 21, skeleton

---
+++ leetcode_july_2020/day21.cpp
@@ -0,0 +1,16 @@
+// Day 21, Word Search
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class SolutionDay21 {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        return true;
+    }
+};
+
+in main() {
+    // Compile successful
+    return 0;
+}
