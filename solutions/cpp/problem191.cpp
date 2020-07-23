695c3eb4e9592239e30263f46c6ccd00c64dfb76
gxyau <GuoXian.Yau@gmail.com>
2020-07-23 17:34:58 +0200

    Problem 191, Count Number of 1 Bits

--- solutions/cpp/problem191.cpp
+++ solutions/cpp/problem191.cpp
@@ -1,11 +1,13 @@
-// Problem 191, Number of 1 Bits                                                                          
+// Problem 191, Number of 1 Bits 
 #include <iostream>    
     
 class Solution191 {    
     public:    
         int hammingWeight(uint32_t n) {    
             int bit = 32, nbits = 0;    
-            while (bit--) nbits += n & (1 << bit);    
+            while (bit--) {
+                if (n & (1 << bit)) ++nbits;
+            }
             return nbits;    
         }    
 };    
@@ -15,7 +17,7 @@ int main() {
     uint32_t n, nbits;    
     std::cout << "Please input a 32-bit binary number: ";    
     std::cin  >> n;    
-    nbits = sol.Solution191(n);    
+    nbits = sol.hammingWeight(n);    
     std::cout << "Number of 1 bits: " << nbits << std::endl;    
     // Compile successful    
     return 0;    
