d7bde9809e55b89dea5a8bc15fe8aa4195bc07f2
gxyau <GuoXian.Yau@gmail.com>
2020-07-24 10:33:28 +0200

    Day 24, Skeleton"

---
+++ leetcode_july_2020/day24.cpp
@@ -0,0 +1,16 @@
+// Day 24, All Paths from Source to Target
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+            return {};
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
