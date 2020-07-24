bbf89819ea5ef9efde08314ff2cc148e84977587
gxyau <GuoXian.Yau@gmail.com>
2020-07-24 10:40:36 +0200

    Added test case

--- leetcode_july_2020/day24.cpp
+++ leetcode_july_2020/day24.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using std::vector;
 
-class Solution {
+class SolutionDay24 {
     public:
         vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
             return {};
@@ -11,6 +11,19 @@ class Solution {
 };
 
 int main() {
+    SolutionDay24 sol;
+    vector<vector<int>> graph, paths;
+    // Test case 1
+    graph = {{1,2},{3},{3},{}};
+    paths = sol.allPathSourceTarget(graph);
+    std::cout << "The paths are: " << std::endl;
+    for (vector<int> vec : paths) {
+        for (int x : vec) {
+            std::cout << x << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
     // Compile successful
     return 0;
 }
