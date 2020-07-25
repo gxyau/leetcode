c1d1bd6d81122e090609ef4a9710d3e14a8ee162
gxyau <GuoXian.Yau@gmail.com>
2020-07-25 09:54:06 +0200

    Added new test case

--- leetcode_july_2020/day25.cpp
+++ leetcode_july_2020/day25.cpp
@@ -7,13 +7,8 @@ using std::vector;
 class SolutionDay25 {
     public:
         int findMin(vector<int>& nums) {
-            vector<int>::iterator it = nums.begin();
-            while (it+1 != nums.end()) {
-                if (*it > *(it+1)) break;
-                ++it;
-            }
-            if (it+1 == nums.end()) return nums[0];
-            return *(it+1);
+            int l = 0, r = nums.size()-1, m = l + (r-l)/2;
+            return nums[l]
         }
 };
 
@@ -22,20 +17,27 @@ int main() {
     vector<int> nums;
     int minimum;
     // Test case 1;
-    nums    = {1,3,5};
+    nums    = {1};
     std::cout << "The vector is: ";
     for (int x : nums) std::cout << x << " ";
     std::cout << std::endl;
     minimum = sol.findMin(nums);
     std::cout << "The minimum is: " << minimum << std::endl;
     // Test case 2;
-    nums    = {2,2,2,0,1};
+    nums    = {1,3,5};
     std::cout << "The vector is: ";
     for (int x : nums) std::cout << x << " ";
     std::cout << std::endl;
     minimum = sol.findMin(nums);
     std::cout << "The minimum is: " << minimum << std::endl;
     // Test case 3;
+    nums    = {2,2,2,0,1};
+    std::cout << "The vector is: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    minimum = sol.findMin(nums);
+    std::cout << "The minimum is: " << minimum << std::endl;
+    // Test case 4;
     nums    = {4,5,5,6,7,8,9,10,11,1,2,3};
     std::cout << "The vector is: ";
     for (int x : nums) std::cout << x << " ";
