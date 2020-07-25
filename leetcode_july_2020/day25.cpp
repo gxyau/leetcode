452efaba8e7c0aacf2c510359b6deb4b152c3ae6
gxyau <GuoXian.Yau@gmail.com>
2020-07-25 09:35:56 +0200

    Day 25, Skeleton

---
+++ leetcode_july_2020/day25.cpp
@@ -0,0 +1,18 @@
+// Day 25, Find Minimum in a Rotated Sorted Array II
+#include <iostream>
+#include <limits>
+#include <vector>
+using std::vector;
+
+class SolutionDay25 {
+    public:
+        int findMin(vector<int>& nums) {
+            int minimum = std::numeric_limits<int>::min();
+            return minimum;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
