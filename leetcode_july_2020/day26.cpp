d87ef509eda8c42eec65babfa7aed843c2bce40f
gxyau <GuoXian.Yau@gmail.com>
2020-07-26 11:40:53 +0200

    Day 26, skeleton

---
+++ leetcode_july_2020/day26.cpp
@@ -0,0 +1,14 @@
+// Day 26, Add Digits
+#include <iostream>
+
+class SolutionDay26 {
+public:
+    int addDigits(int num) {
+        return 0;
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
