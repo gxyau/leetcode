d654718fddac52f28238fac684e6e06cf70e8524
gxyau <GuoXian.Yau@gmail.com>
2020-07-26 11:53:56 +0200

    Day 26, Add digits, O(1) solution optimized

--- leetcode_july_2020/day26.cpp
+++ leetcode_july_2020/day26.cpp
@@ -4,8 +4,7 @@
 class SolutionDay26 {
     public:
         int addDigits(int num) {
-            if (num == 0) return 0;
-            return 1 + (num-1)%9;
+            return num ? 1 + (num-1)%9 : 0;
         }
 };
 
