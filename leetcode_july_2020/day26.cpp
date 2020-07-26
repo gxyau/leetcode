a5c0daa59255b338811b39066ba6b9b4b4cecbb5
gxyau <GuoXian.Yau@gmail.com>
2020-07-26 11:44:07 +0200

    Day 26, test solution

--- leetcode_july_2020/day26.cpp
+++ leetcode_july_2020/day26.cpp
@@ -4,11 +4,20 @@
 class SolutionDay26 {
 public:
     int addDigits(int num) {
-        return 0;
+        while(num / 10) {
+            num = (num/10) + (num%10);
+        }
+        return num;
     }
 };
 
 int main() {
+    SolutionDay26 sol;
+    int num, ans;
+    std::cout << "Please enter a number: ";
+    std::cin  >> num;
+    ans = sol.addDigits(num);
+    std::cout << "The digits sum is: " << ans << std::endl;
     // Compile successful
     return 0;
 }
