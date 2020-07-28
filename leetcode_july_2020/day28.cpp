1136343702cfeb499f1f037878222f4c335694df
gxyau <GuoXian.Yau@gmail.com>
2020-07-28 11:58:32 +0200

    Day 28, Task Scheduler

--- leetcode_july_2020/day28.cpp
+++ leetcode_july_2020/day28.cpp
@@ -4,10 +4,21 @@
 using std::vector;
 
 class SolutionDay28 {
-public:
-    int leastInterval(vector<char>& tasks, int n) {
-        return 0;
-    }
+    public:
+        int leastInterval(vector<char>& tasks, int n) {
+            if (!n) return tasks.size();
+            int freq[26] = {0}, max_freq = 0, num_max = 0;
+            // Saving frequency table, and maximum frequency
+            for (char x : tasks) max_freq = std::max(max_freq, ++freq[x-'A']);
+            // Finding number of elements with maximum frequency
+            for (int i = 0; i < 26; ++i) if (freq[i] == max_freq) ++num_max;
+            // Idle time not processing one of the maximum frequency elements
+            int idle = n + 1 - num_max;
+            // More max freq elms than waiting time
+            if (idle <= 0) return tasks.size();
+            // Otherwise
+            return std::max((int) tasks.size(), max_freq * num_max + idle * (max_freq - 1));
+        }
 };
 
 int main() {
