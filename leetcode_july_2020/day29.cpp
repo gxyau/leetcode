bce04c5184839e52f3e4bb4ae9c6269fd924eecc
gxyau <GuoXian.Yau@gmail.com>
2020-07-29 11:05:54 +0200

    Day29, Added test cases

--- leetcode_july_2020/day29.cpp
+++ leetcode_july_2020/day29.cpp
@@ -13,6 +13,18 @@ int main() {
     SolutionDay29 sol;
     vector<int> prices;
     int ans;
+    // Test case 1
+    prices = {1};
+    ans = sol.maxProfit(prices);
+    std::cout << "The max profit is: " << ans << std::endl;
+    // Test case 2
+    prices = {1,2};
+    ans = sol.maxProfit(prices);
+    std::cout << "The max profit is: " << ans << std::endl;
+    // Test case 3
+    prices = {1,2,3,0,2};
+    ans = sol.maxProfit(prices);
+    std::cout << "The max profit is: " << ans << std::endl;
     // Compile successful
     return 0;
 }
