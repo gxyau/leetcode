488e1119f8f39e06c44d5977fcbdd4bd82278ed0
gxyau <GuoXian.Yau@gmail.com>
2020-07-29 11:51:09 +0200

    Day 29, Best Time to buy and sell stock with cooldown

--- leetcode_july_2020/day29.cpp
+++ leetcode_july_2020/day29.cpp
@@ -1,12 +1,21 @@
 // Day 29, Best Time to Buy and Sell Stock with Cooldown
 #include <iostream>
+#include <limits>
 #include <vector>
+using std::vector;
 
 class SolutionDay29 {
-public:
-    int maxProfit(vector<int>& prices) {
-        return 0;
-    }
+    public:
+        int maxProfit(vector<int>& prices) {    
+            int b = std::numeric_limits<int>::min(),s = 0,p_sell = 0,p_buy;
+            for(int stock : prices){
+                p_buy  = b;
+                b      = max(b, p_sell - stock);
+                p_sell = s;
+                s      = max(s, p_buy + stock);
+            }
+            return s;
+        }
 };
 
 int main() {
