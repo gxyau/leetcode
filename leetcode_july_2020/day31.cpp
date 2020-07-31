91c4a21c7d7244ac31252b2b6cbc05237c8d7bbf
gxyau <GuoXian.Yau@gmail.com>
2020-07-31 11:53:41 +0200

    Day 31, Climb Stairs, Fibonacci

--- leetcode_july_2020/day31.cpp
+++ leetcode_july_2020/day31.cpp
@@ -6,11 +6,22 @@ using std::vector;
 class SolutionDay31 {
 public:
     int climbStairs(int n) {
-        vector<int> DP;
+        if (n <= 2) return n;
+        vector<int> DP = {1,2};
+        for (int i = 2; i < n; ++i) {
+            DP.push_back(DP[i-1] + DP[i-2]);
+        }
         return DP[DP.size()-1];
     }
 };
 
 int main() {
+    SolutionDay31 sol;
+    int n, ans;
+    std::cout << "Please enter a positive integer between 1 and 45 (inclusive): ";
+    std::cin  >> n;
+    ans = sol.climbStairs(n);
+    std::cout << "Total number of ways of climbing the stairs: " << ans << std::endl;
+    // Compile successful
     return 0;
 }
