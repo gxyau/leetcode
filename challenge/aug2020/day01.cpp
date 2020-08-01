4e3bdc2c052c08e21da91011f07faa28cd1667e0
gxyau <GuoXian.Yau@gmail.com>
2020-08-01 11:57:19 +0200

    Day 1, skeleton

---
+++ challenge/aug2020/day01.cpp
@@ -0,0 +1,16 @@
+// Day 1, Detect Capital
+#include <iostream>
+#include <string>
+using std::string;
+
+class SolutionDay1 {
+    public:
+        bool detectCapitalUse(string word) {
+            return true;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
