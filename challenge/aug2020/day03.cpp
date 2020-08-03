317b37cf6e6294fbcb3d8d13c476ca1673a2f4dd
gxyau <GuoXian.Yau@gmail.com>
2020-08-03 11:10:27 +0200

    Day 3, skeleton

---
+++ challenge/aug2020/day03.cpp
@@ -0,0 +1,15 @@
+// Day 3, Valid palindrome
+#include <string>
+using std::string;
+
+class SolutionDay3 {
+    public:
+        bool isPalindrome(string s) {
+            return true;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
