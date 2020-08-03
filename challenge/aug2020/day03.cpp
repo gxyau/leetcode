7c4642db896250164cea5d8b967907233a431655
gxyau <GuoXian.Yau@gmail.com>
2020-08-03 11:13:16 +0200

    Day 3, added basic structure, implementing now

--- challenge/aug2020/day03.cpp
+++ challenge/aug2020/day03.cpp
@@ -1,4 +1,5 @@
 // Day 3, Valid palindrome
+#include <iostream>
 #include <string>
 using std::string;
 
@@ -10,6 +11,13 @@ class SolutionDay3 {
 };
 
 int main() {
+    SolutionDay3 sol;
+    string s;
+    bool ans;
+    std::cout << "Please enter a string: ";
+    std::cin  >> s;
+    ans = sol.isPalindrome(s);
+    std::cout << "is the string a palindrome?: " << ans << std::endl;
     // Compile successful
     return 0;
 }
