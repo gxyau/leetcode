f0191180b8d130f8581a9b6dca90bdac0a050a24
gxyau <GuoXian.Yau@gmail.com>
2020-08-03 11:18:19 +0200

    Day 3, Using iterators, one pass, needs debugging

--- challenge/aug2020/day03.cpp
+++ challenge/aug2020/day03.cpp
@@ -1,11 +1,20 @@
 // Day 3, Valid palindrome
 #include <iostream>
 #include <string>
+using std::isalnum;
 using std::string;
 
 class SolutionDay3 {
     public:
         bool isPalindrome(string s) {
+            string::iterator it1 = s.begin(), it2 = s.end()-1;
+            while (it1 < it2) {
+                while (!isalnum(*it1)) ++it1;
+                while (!isalnum(*it2)) --it2;
+                if (*it1 != *it2) return false;
+                ++it1;
+                --it2;
+            }
             return true;
         }
 };
