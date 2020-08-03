ef04774bfc01b0190058f73dece137de101db93e
gxyau <GuoXian.Yau@gmail.com>
2020-08-03 11:23:46 +0200

    Day 3, Valid palindrome, Iterator, one pass, case insensitive

--- challenge/aug2020/day03.cpp
+++ challenge/aug2020/day03.cpp
@@ -3,16 +3,16 @@
 #include <string>
 using std::isalnum;
 using std::string;
+using std::tolower;
 
 class SolutionDay3 {
     public:
         bool isPalindrome(string s) {
             string::iterator it1 = s.begin(), it2 = s.end()-1;
-            // Start from both ends, check until they meet
             while (it1 < it2) {
                 while (!isalnum(*it1)) ++it1;
                 while (!isalnum(*it2)) --it2;
-                if (*it1 != *it2) return false;
+                if (tolower(*it1) != tolower(*it2)) return false;
                 ++it1;
                 --it2;
             }
