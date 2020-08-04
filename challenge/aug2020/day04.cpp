f173654ff9a4b3186d77048800903d50ec8ef7bf
gxyau <GuoXian.Yau@gmail.com>
2020-08-04 10:24:45 +0200

    Compile successful

--- challenge/aug2020/day04.cpp
+++ challenge/aug2020/day04.cpp
@@ -1,10 +1,24 @@
 // Day 4, Power of Four
-class Solution {
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class SolutionDay4 {
     public:
         bool isPowerOfFour(int num) {
+            return true;
         }
 };
 
 int main() {
+    SolutionDay4 sol;
+    int num;
+    bool ans;
+    cout << "Please enter a positive integer";
+    cin  >> num;
+    ans  = sol.isPowerOfFour(num);
+    cout << "The number is a power of four? " << ans << endl;
+    // Compile successful
     return 0;
 }
