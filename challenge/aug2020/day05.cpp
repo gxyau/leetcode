532ded19e5b611ae3a89a470647fb9222bcd45aa
gxyau <GuoXian.Yau@gmail.com>
2020-08-05 09:08:53 +0200

    Day 5, skeleton

---
+++ challenge/aug2020/day05.cpp
@@ -0,0 +1,27 @@
+// Day 5, Add and Search Word, Data Structure design
+#include <iostream>
+#include <string>
+using std::string;
+
+class WordDictionary {
+    public:
+        /** Initialize your data structure here. */
+        WordDictionary() {
+
+        }
+
+        /** Adds a word into the data structure. */
+        void addWord(string word) {
+
+        }
+
+        /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+        bool search(string word) {
+
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
