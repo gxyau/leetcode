ab2c941732d111ad2365883f45220b20a3b92ff8
gxyau <GuoXian.Yau@gmail.com>
2020-08-06 09:39:43 +0200

    Added test cases

--- challenge/aug2020/day06.cpp
+++ challenge/aug2020/day06.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using std::vector;
 
-class Solution {
+class Solutionday6 {
     public:
         vector<int> findDuplicates(vector<int>& nums) {
             return {};
@@ -11,5 +11,44 @@ class Solution {
 };
 
 int main() {
+    SolutionDay6 sol;
+    vector<int> nums, duplicates;
+    // Test case 1
+    nums = {};
+    std::cout << "The vector is: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << "\n";}
+    duplicates = sol.findDuplicates(nums);
+    std::cout << "The duplicates are: ";
+    for (int x : duplicates) std::cout << x << " ";
+    std::cout << "\n";
+    // Test case 2
+    nums = {1};
+    std::cout << "The vector is: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << "\n";}
+    duplicates = sol.findDuplicates(nums);
+    std::cout << "The duplicates are: ";
+    for (int x : duplicates) std::cout << x << " ";
+    std::cout << "\n";
+    // Test case 1
+    nums = {1,1};
+    std::cout << "The vector is: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << "\n";}
+    duplicates = sol.findDuplicates(nums);
+    std::cout << "The duplicates are: ";
+    for (int x : duplicates) std::cout << x << " ";
+    std::cout << "\n";
+    // Test case 1
+    nums = {4,3,2,7,8,2,3,1};
+    std::cout << "The vector is: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << "\n";}
+    duplicates = sol.findDuplicates(nums);
+    std::cout << "The duplicates are: ";
+    for (int x : duplicates) std::cout << x << " ";
+    std::cout << "\n";
+    // Compile successful
     return 0;
 }
