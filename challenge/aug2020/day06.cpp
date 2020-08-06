24d7c473ba755c152620e06b8603c1da247b5bd2
gxyau <GuoXian.Yau@gmail.com>
2020-08-06 09:34:20 +0200

    Day 6, Skeleton

---
+++ challenge/aug2020/day06.cpp
@@ -0,0 +1,15 @@
+// Day 6, Find All Duplicates in an Array
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        vector<int> findDuplicates(vector<int>& nums) {
+            return {};
+        }
+};
+
+int main() {
+    return 0;
+}
