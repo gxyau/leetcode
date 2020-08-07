c7735b26b88c91e68e30c9b412bde667d11cc34c
gxyau <GuoXian.Yau@gmail.com>
2020-08-07 11:39:31 +0200

    Day 7, skeleton

---
+++ challenge/aug2020/day07.cpp
@@ -0,0 +1,26 @@
+// Day 7, Vertical Order Traversal of a Binary Tree
+#include <iostream>
+#include <vector>
+using std::vector;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
