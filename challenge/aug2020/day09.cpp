0500f1f48b1917598f114480f6bc5889ea750fc1
gxyau <GuoXian.Yau@gmail.com>
2020-08-09 11:03:00 +0200

    Day 9, skeleton

---
+++ challenge/aug2020/day09.cpp
@@ -0,0 +1,17 @@
+// Day 9, Rotting Oranges
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int orangesRotting(vector<vector<int>>& grid) {
+            int minutes = 0;
+            return minutes;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
