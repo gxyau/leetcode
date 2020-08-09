010f9a5ca1bac71f06b0dea2446399da12eb5a56
gxyau <GuoXian.Yau@gmail.com>
2020-08-09 11:18:41 +0200

    Added two new test cases

--- challenge/aug2020/day09.cpp
+++ challenge/aug2020/day09.cpp
@@ -17,8 +17,8 @@ int main() {
     int minutes;
     // Test case 1
     grid = {{1}};
-    std::cout << "The grid is:\n";
-    for (vec<int> row : grid) {
+    std::cout << "The grid is:\n\n";
+    for (vector<int> row : grid) {
         for (int x : row) std::cout << x << "  ";
         std::cout << std::endl;
     }
@@ -27,8 +27,8 @@ int main() {
     std::cout << "Number of minutes: " << minutes << "\n\n";
     // Test case 2
     grid = {{1,2}};
-    std::cout << "The grid is:\n";
-    for (vec<int> row : grid) {
+    std::cout << "The grid is:\n\n";
+    for (vector<int> row : grid) {
         for (int x : row) std::cout << x << "  ";
         std::cout << std::endl;
     }
@@ -37,8 +37,8 @@ int main() {
     std::cout << "Number of minutes: " << minutes << "\n\n";
     // Test case 3
     grid = {{1,0}};
-    std::cout << "The grid is:\n";
-    for (vec<int> row : grid) {
+    std::cout << "The grid is:\n\n";
+    for (vector<int> row : grid) {
         for (int x : row) std::cout << x << "  ";
         std::cout << std::endl;
     }
@@ -47,8 +47,28 @@ int main() {
     std::cout << "Number of minutes: " << minutes << "\n\n";
     // Test case 4
     grid = {{2,1,1},{0,1,1},{1,0,1}};
-    std::cout << "The grid is:\n";
-    for (vec<int> row : grid) {
+    std::cout << "The grid is:\n\n";
+    for (vector<int> row : grid) {
+        for (int x : row) std::cout << x << "  ";
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    minutes = sol.orangesRotting(grid);
+    std::cout << "Number of minutes: " << minutes << "\n\n";
+    // Test case 5
+    grid = {{2,0,1},{0,0,1},{1,1,1}};
+    std::cout << "The grid is:\n\n";
+    for (vector<int> row : grid) {
+        for (int x : row) std::cout << x << "  ";
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    minutes = sol.orangesRotting(grid);
+    std::cout << "Number of minutes: " << minutes << "\n\n";
+    // Test case 6
+    grid = {{2,0,1},{0,1,1},{1,1,1}};
+    std::cout << "The grid is:\n\n";
+    for (vector<int> row : grid) {
         for (int x : row) std::cout << x << "  ";
         std::cout << std::endl;
     }
