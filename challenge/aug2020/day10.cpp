619c4043bb488c0110eac7d41a11f061489e7198
gxyau <GuoXian.Yau@gmail.com>
2020-08-10 12:26:55 +0200

    Day 10, skeleton

---
+++ challenge/aug2020/day10.cpp
@@ -0,0 +1,17 @@
+// Day 10, Excel Sheet Column Number
+#include <iostream>
+#include <string>
+using std::string;
+
+class SolutionDay10 {
+    public:
+        int titleToNumber(string s) {
+            int column = 0;
+            return column;
+        }
+};
+
+int main() {
+    // Compile successfully
+    return 0;
+}
