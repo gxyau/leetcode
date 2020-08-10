3142a825787b368295535366264fd0804dbba94b
gxyau <GuoXian.Yau@gmail.com>
2020-08-10 12:45:35 +0200

    Added basic structure, compiles successfully

--- challenge/aug2020/day10.cpp
+++ challenge/aug2020/day10.cpp
@@ -1,4 +1,5 @@
 // Day 10, Excel Sheet Column Number
+#include <algorithm>
 #include <iostream>
 #include <string>
 using std::string;
@@ -12,6 +13,15 @@ class SolutionDay10 {
 };
 
 int main() {
+    SolutionDay10 sol;
+    int column;
+    string s;
+    std::cout << "Please enter a column between \"A\" and \"FXSHRXW\": ";
+    std::cin  >> s;
+    // Make sure it is upper case
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {return std::toupper(c);});
+    column = sol.titleToNumber(s);
+    std::cout << "It is column number of " << s << " is: " << column << std::endl;
     // Compile successfully
     return 0;
 }
