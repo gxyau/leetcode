6424b5efc58f68b77d99a683b0a785e7337042fb
gxyau <GuoXian.Yau@gmail.com>
2020-08-11 10:01:40 +0200

    Day 11, skeleton

---
+++ challenge/aug2020/day11.cpp
@@ -0,0 +1,18 @@
+// Day 11, H Index
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int hIndex(vector<int>& citations) {
+            int index;
+            return index;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
