223bca8489e4e62a4062b5c1fbfeecdbeaeb2c63
gxyau <GuoXian.Yau@gmail.com>
2020-08-12 10:29:11 +0200

    Problem 118, Pascal's triangle

---
+++ solutions/cpp/problem118.cpp
@@ -0,0 +1,37 @@
+// Problem 118, Pascal Triangle
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution118 {
+    public:
+        vector<vector<int>> getRow(int k) {
+            vector<vector<int>> pascal(k+1);
+            vector<int> row;
+            for (int nrow = 0; nrow <= k; ++nrow) {
+                row = vector<int>(nrow+1, {0});
+                row[0] = 1;
+                for (int i = 1; i < k+1; ++i) {
+                    for (int j = i ; j >= 1; --j) {
+                        row[j] += row[j-1];
+                    }
+                }
+                pascal.push_back(row);
+            }
+            return pascal;
+        }
+};
+
+int main() {
+    Solution118 sol;
+    int k;
+    vector<vector<int>> pascal;
+    std::cout << "Please enter a number between 0 and 33 (inclusive): ";
+    std::cin  >> k;
+    pascal = sol.getRow(k);
+    std::cout << "Row " << k << " looks like: ";
+    for (int x : pascal) std::cout << x << " ";
+    std::cout << std::endl;
+    // Compile successfully
+    return 0;
+}
