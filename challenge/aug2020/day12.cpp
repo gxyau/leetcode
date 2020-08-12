d112cbc4ef6d5946a3e7bd1ed9f224646c475ba3
gxyau <GuoXian.Yau@gmail.com>
2020-08-12 09:32:37 +0200

    Day 12, skeleton

---
+++ challenge/aug2020/day12.cpp
@@ -0,0 +1,17 @@
+// Day 12, Pascal Triangle II
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        vector<int> getRow(int rowIndex) {
+            vector<int> pascal;
+            return pascal;
+        }
+};
+
+int main() {
+    // Compile successfully
+    return 0;
+}
