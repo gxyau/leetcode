f9bf32ebbf82cf0c124df7daf363e9713586959d
gxyau <GuoXian.Yau@gmail.com>
2020-08-12 10:06:28 +0200

    Very inefficient solution and might have runtime error or overflow error

--- challenge/aug2020/day12.cpp
+++ challenge/aug2020/day12.cpp
@@ -4,21 +4,29 @@
 using std::vector;
 
 class SolutionDay12 {
+    private:
+        int choose(int n, int k, int num, int denom) {
+            if (k == 0) return (int) num/denom;
+            return choose(n-1,k-1,num*n, denom*k);
+        }
     public:
-        vector<int> getRow(int rowIndex) {
+        vector<int> getRow(int k) {
             vector<int> pascal;
+            for (int i = 0; i < k+1; ++i) {
+                pascal.push_back(choose(k,i,1,1));
+            }
             return pascal;
         }
 };
 
 int main() {
     SolutionDay12 sol;
-    int row;
+    int k;
     vector<int> pascal;
     std::cout << "Please enter a number between 0 and 33 (inclusive): ";
-    std::cin  >> row;
-    pascal = sol.getRow(row);
-    std::cout << "Row " << row << " looks like: ";
+    std::cin  >> k;
+    pascal = sol.getRow(k);
+    std::cout << "Row " << k << " looks like: ";
     for (int x : pascal) std::cout << x << " ";
     std::cout << std::endl;
     // Compile successfully
