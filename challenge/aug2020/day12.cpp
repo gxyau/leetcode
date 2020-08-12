d74a3a9c1cf12475fd5bb1041b20ee552ebf5eb4
gxyau <GuoXian.Yau@gmail.com>
2020-08-12 09:36:02 +0200

    Compile successful, made a simple program

--- challenge/aug2020/day12.cpp
+++ challenge/aug2020/day12.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using std::vector;
 
-class Solution {
+class SolutionDay12 {
     public:
         vector<int> getRow(int rowIndex) {
             vector<int> pascal;
@@ -12,6 +12,15 @@ class Solution {
 };
 
 int main() {
+    SolutionDay12 sol;
+    int row;
+    vector<int> pascal;
+    std::cout << "Please enter a number between 0 and 33 (inclusive): ";
+    std::cin  >> row;
+    pascal = sol.getRow(row);
+    std::cout << "Row " << row << " looks like: ";
+    for (int x : pascal) std::cout << x << " ";
+    std::cout << std::endl;
     // Compile successfully
     return 0;
 }
