4c10e4927ca2ec47e5f9e93baa89900e66c6fc62
gxyau <GuoXian.Yau@gmail.com>
2020-08-14 09:41:38 +0200

    Day 14, skeleton

---
+++ challenge/aug2020/day14.cpp
@@ -0,0 +1,16 @@
+// Day 14, Longest Palindrome
+#include <iostream>
+#include <string>
+using std::string;
+
+class Solution {
+    public:
+        int longestPalindrome(string s) {
+            return 0;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
