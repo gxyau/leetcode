2d108b2777ecbf3a430552df40ab0aa4dbc29918
gxyau <GuoXian.Yau@gmail.com>
2020-08-14 10:05:59 +0200

    Added small program

--- challenge/aug2020/day14.cpp
+++ challenge/aug2020/day14.cpp
@@ -3,14 +3,31 @@
 #include <string>
 using std::string;
 
-class Solution {
+class SolutionDay14 {
     public:
+        bool palindrome(string s) {
+            if (s.size() <= 1) return true;
+            int n = s.size();
+            return s[0] == s[n-1] ? palindrome(s.substr(1, n-2)) : false;
+        }
+        
         int longestPalindrome(string s) {
-            return 0;
+            string substr;
+            int len = 0;
+            for (char c : s) {
+            }
+            return len;
         }
 };
 
 int main() {
+    SolutionDay14 sol;
+    string s;
+    int len;
+    std::cout << "Please enter a string: ";
+    std::cin  >> s;
+    len = sol.longestPalindrome(s);
+    std::cout << "The longest palindrome has length: " << len << std::endl;
     // Compile successful
     return 0;
 }
