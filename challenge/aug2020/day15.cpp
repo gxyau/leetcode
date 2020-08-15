8520902a701266c269c2953008a7815f99362ef9
gxyau <GuoXian.Yau@gmail.com>
2020-08-15 09:56:02 +0200

    Day 15, skeleton

---
+++ challenge/aug2020/day15.cpp
@@ -0,0 +1,16 @@
+// Day 15, Non-overlapping Intervals
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+            return 0;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
