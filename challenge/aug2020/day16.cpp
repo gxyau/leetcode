3d899de1a1a8dee42afb39780d131f434f2b17d9
gxyau <GuoXian.Yau@gmail.com>
2020-08-16 11:49:29 +0200

    Day 16, Best Time to Buy and Sell Stocks III

--- challenge/aug2020/day16.cpp
+++ challenge/aug2020/day16.cpp
@@ -1,13 +1,23 @@
 // Day 16, Best Time to Buy and Sell Stock III
 #include <iostream>
 #include <vector>
+using std::vector;
 
 class Solution {
     public:
         int maxProfit(vector<int>& prices) {
-            return 0;
-        }
-};
+            int max1 = 0, max2 = 0;
+            int price1 = std::numeric_limits<int>::max();
+            int price2 = std::numeric_limits<int>::max();
+            
+            for (int p : prices) {
+                price1 = std::min(price1, p);
+                max1   = std::max(max1, p - price1);
+                price2 = std::min(price2, p - max1);
+                max2   = std::max(max2, p - price2);
+            }
+            
+            return max2;
 
 int main() {
     // Compile successful
