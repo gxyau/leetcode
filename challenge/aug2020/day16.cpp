7c9f827711a615813929f0f1ba53987015447c89
gxyau <GuoXian.Yau@gmail.com>
2020-08-16 11:01:07 +0200

    Day 16, skeleton

---
+++ challenge/aug2020/day16.cpp
@@ -0,0 +1,15 @@
+// Day 16, Best Time to Buy and Sell Stock III
+#include <iostream>
+#include <vector>
+
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices) {
+            return 0;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
