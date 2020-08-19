4cee88966bb9ca5ae4800f2ffc35a6db382522ff
gxyau <GuoXian.Yau@gmail.com>
2020-08-19 09:52:00 +0200

    Day 19, skeleton

---
+++ challenge/aug2020/day19.cpp
@@ -0,0 +1,19 @@
+// Day 19, Goat Latin
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using std::string;
+using std::unordered_set;
+
+class Solution {
+    public:
+        string toGoatLatin(string S) {
+            string T;
+            return T;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
