f5464996e8f7494a209f8b2bae8c4b5798445377
gxyau <GuoXian.Yau@gmail.com>
2020-08-21 10:09:09 +0200

    Day 21, skeleton

---
+++ challenge/aug2020/day21.cpp
@@ -0,0 +1,15 @@
+// Day 21, Array sort by parity
+#include <iostream>
+#include <vector>
+using std::vector;
+class SolutionDay21 {
+    public:
+        vector<int> sortArrayByParity(vector<int>& A) {
+
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
