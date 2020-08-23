b2facd9c4f69d86457fa93174f355f14ce7694ce
gxyau <GuoXian.Yau@gmail.com>
2020-08-23 12:29:57 +0200

    Day 23, Stream of Characters

--- challenge/aug2020/day23.cpp
+++ challenge/aug2020/day23.cpp
@@ -12,35 +12,55 @@ struct TrieNode {
 class StreamChecker {
     private:
         TrieNode* root = nullptr;
+        int max_length = 0;
+        std::deque<char> dq;
         
-        void add_word(string &word) {
+        void insert(string &word) {
             if (word == "") return;
-            TrieNode *cur = root;
+            TrieNode* cur = root;
             for (char c : word) {
                 if (!(cur->child[c-'a'])) cur->child[c-'a'] = new TrieNode();
                 cur = cur->child[c-'a'];
             }
             cur->end = true;
         }
+        
+        void DFS(deque<char> dq, TrieNode* ptr) {
+            
+        }
     public:
         StreamChecker(vector<string>& words) {
             root = new TrieNode();
             for (string word : words) {
-                add_word(word);
+                max_length = std::max(max_length, (int) word.size());
+                insert(word);
             }
         }
 
         bool query(char letter) {
+            dq.push_back(letter);
+            if (dq.size() > max_length) dq.pop_front();
             TrieNode* cur = root;
-            for (char c : letter) {
-                if (!cur->child[c-'a']) return false;
-                cur = cur->child[c-'a'];
-            };
-            return true;
+            auto ptr = dq.rbegin();
+            while(ptr != dq.rend()){
+                if(cur->child[*ptr - 'a'] == nullptr)
+                    return false;
+                cur = cur->child[*ptr - 'a'];
+                if(cur->end)
+                    return true;
+                ++ptr;
+            }
+            return false;
         }
 };
 
-\int main() {
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
+
+int main() {
     // Compile successful
     return 0;
 }
