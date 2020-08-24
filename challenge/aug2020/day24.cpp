8a6acbad76f9860be2acd7cf6bf4c6261bac3cf5
gxyau <GuoXian.Yau@gmail.com>
2020-08-24 11:09:26 +0200

    Day 24, skeleton

---
+++ challenge/aug2020/day24.cpp
@@ -0,0 +1,25 @@
+// Day 24, Sum of left leaves;
+#include <iostream>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class SolutionDay24 {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        return 0;
+    }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
