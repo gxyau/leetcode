6973a24af75665522cac65ac80facfb96f357794
gxyau <GuoXian.Yau@gmail.com>
2020-08-24 11:25:01 +0200

    Day 24, Sum of Left Leaf

--- challenge/aug2020/day24.cpp
+++ challenge/aug2020/day24.cpp
@@ -13,10 +13,22 @@
  * };
  */
 class SolutionDay24 {
-public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        return 0;
-    }
+    private:
+        int left_sum = 0;
+        
+        void find_left_leaf(TreeNode* node, bool is_left) {
+            if (!node) return;
+            if (!(node->left) && !(node->right)) {
+                left_sum += is_left ? node->val : 0;
+            }
+            find_left_leaf(node->left, true);
+            find_left_leaf(node->right, false);
+        }
+    public:
+        int sumOfLeftLeaves(TreeNode* root) {
+            find_left_leaf(root, false);
+            return left_sum;
+        }
 };
 
 int main() {
