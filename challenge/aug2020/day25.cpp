ea9b59f38cd31d332e415b4e72bdca280b31dcdb
gxyau <GuoXian.Yau@gmail.com>
2020-08-25 11:06:12 +0200

    Day 25 Skeleton

---
+++ challenge/aug2020/day25.cpp
@@ -0,0 +1,16 @@
+// Day 25, Minimum Cost for Tickets
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int mincostTickets(vector<int>& days, vector<int>& costs) {
+            return 0;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
