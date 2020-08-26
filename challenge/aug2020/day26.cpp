78fa1dcbb89388516dc57c68a50574c3c65a7bae
gxyau <GuoXian.Yau@gmail.com>
2020-08-26 10:57:03 +0200

    Day 26, skeleton

---
+++ challenge/aug2020/day26.cpp
@@ -0,0 +1,18 @@
+// Day 26, Fizz Buzz
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class SolutionDay26 {
+    public:
+        vector<string> fizzBuzz(int n) {
+            return {};
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
