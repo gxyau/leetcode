46550e9934192b8115b3e23f007598b505c27c89
gxyau <GuoXian.Yau@gmail.com>
2020-08-27 12:01:49 +0200

    Day 27, Find Right Interval, Map + Binary search

--- challenge/aug2020/day27.cpp
+++ challenge/aug2020/day27.cpp
@@ -1,12 +1,23 @@
 // Day 27, Find Right Interval
+#include <algorithm>
 #include <iostream>
+#include <map>
 #include <vector>
 using std::vector;
 
 class SolutionDay27 {
     public:
         vector<int> findRightInterval(vector<vector<int>>& intervals) {
-            vector<int> right_intervals = {};
+            vector<int> right_intervals(intervals.size(), {-1});
+            std::map<int, int> order;
+            std::map<int, int>::iterator it;
+            int index = 0;
+            // Sorting intervals
+            for (vector<int> x : intervals) order.insert(std::pair(x[0], index++));
+            for (int i = 0; i < intervals.size(); ++i) {
+                it = order.lower_bound(intervals[i][1]);
+                if (it != order.end()) right_intervals[i] = it->second;                                                                                                          
+            }
             return right_intervals;
         }
 };
