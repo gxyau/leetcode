fbde461d2252332388aa17a957dd9ecf792609dd
gxyau <GuoXian.Yau@gmail.com>
2020-08-27 10:52:34 +0200

    Day 27, skeleton

---
+++ challenge/aug2020/day27.cpp
@@ -0,0 +1,17 @@
+// Day 27, Find Right Interval
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class SolutionDay27 {
+    public:
+        vector<int> findRightInterval(vector<vector<int>>& intervals) {
+            vector<int> right_intervals = {};
+            return right_intervals;
+        }
+};
+
+int main() {
+    // Compile succesful
+    return 0;
+}
