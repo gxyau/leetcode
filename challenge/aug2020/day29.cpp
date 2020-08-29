32dd73a3e2e89332e048b1791a92525ebfdf890a
gxyau <GuoXian.Yau@gmail.com>
2020-08-29 11:07:58 +0200

    Day 29, skeleton

---
+++ challenge/aug2020/day29.cpp
@@ -0,0 +1,17 @@
+// Day 29, Pancake Sorting
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        vector<int> pancakeSort(vector<int>& A) {
+            vector<int> flips;
+            return flips;
+        }
+};
+
+int main() {
+    // Compiles successful
+    return 0;
+}
