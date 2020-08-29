38c8a35ff920c9772bc3d4aa676fecc0706b9931
gxyau <GuoXian.Yau@gmail.com>
2020-08-29 12:27:39 +0200

    Day 29, Pancake Sorting

--- challenge/aug2020/day29.cpp
+++ challenge/aug2020/day29.cpp
@@ -1,14 +1,40 @@
 // Day 29, Pancake Sorting
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using std::vector;
 
 class Solution {
+    private:
+        bool is_sorted(vector<int> &A) {
+            vector<int>::iterator it = A.begin()+1;
+            while (it != A.end()) {
+                if (*it < *(it-1)) return false;
+                ++it;
+            }
+            return true;
+        }
     public:
         vector<int> pancakeSort(vector<int>& A) {
             // Base case, one pacake
             if (A.size() == 1) return {};
+            // Flip history
             vector<int> flips;
+            // Finding max element and its position
+            int max_elm = A.size();
+            vector<int>::iterator it, jt = A.end();
+            while (!is_sorted(A)) {
+                if (max_elm != *(jt-1)) {
+                    it = A.begin();
+                    while (*it != max_elm) ++it;
+                    flips.push_back(it - A.begin() +1);
+                    std::reverse(A.begin(), it+1);
+                    flips.push_back(jt - A.begin());
+                    std::reverse(A.begin(), jt);
+                }
+                --max_elm;
+                --jt;
+            }
             return flips;
         }
 };
