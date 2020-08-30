ad2a505a6ac00e421f5f7e9813e9d850d208701b
gxyau <GuoXian.Yau@gmail.com>
2020-08-30 10:50:06 +0200

    Day 30, skeleton

---
+++ challenge/aug2020/day30.cpp
@@ -0,0 +1,16 @@
+// Day 30, Largest Component Size by Common Factor
+#include <iostring>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int largestComponentSize(vector<int>& A) {
+            return 0;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
