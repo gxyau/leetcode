09284a9ada34f5b13fafe08982922ff4e3ba019f
gxyau <GuoXian.Yau@gmail.com>
2020-08-30 11:01:00 +0200

    Added test cases

--- challenge/aug2020/day30.cpp
+++ challenge/aug2020/day30.cpp
@@ -1,6 +1,8 @@
 // Day 30, Largest Component Size by Common Factor
 #include <iostring>
+#include <numeric>
 #include <vector>
+using std::gcd;
 using std::vector;
 
 class Solution {
@@ -11,6 +13,58 @@ class Solution {
 };
 
 int main() {
+    Solution sol;
+    vector<int> A;
+    int max_comp;
+    // Test case 1;
+    A = {};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
+    // Test case 2;
+    A = {1};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
+    // Test case 3;
+    A = {1,2};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
+    // Test case 4;
+    A = {1,2,3,6};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
+    // Test case 5;
+    A = {4,6,15,35};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
+    // Test case 6;
+    A = {20,50,9,63};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
+    // Test case 7;
+    A = {2,3,6,7,4,12,21,39};
+    std::cout << "List of vertices: ";
+    for (int a : A) std::cout << a << " ";
+    std::cout << std::endl;
+    max_comp = sol.largestComponentSize(A);
+    std::cout << "The largest component size is: " << max_comp << std::endl << std::endl;
     // Compile successful
     return 0;
 }
