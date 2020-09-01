c57893638b5ea39ab9d2c02af575b5b2d8a9b349
gxyau <GuoXian.Yau@gmail.com>
2020-09-01 10:18:51 +0200

    Day 1, skeleton

---
+++ challenge/sep2020/day01.cpp
@@ -0,0 +1,19 @@
+// Day 1, Largest Time for Given Digits
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class Solution {
+    public:
+        string largestTimeFromDigits(vector<int>& A) {
+            string time = "";
+            return time;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
