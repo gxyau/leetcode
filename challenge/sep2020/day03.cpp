be49de96567156a5c9cfeb79af9ac671182c081d
gxyau <GuoXian.Yau@gmail.com>
2020-09-03 11:04:57 +0200

    Added small program

--- challenge/sep2020/day03.cpp
+++ challenge/sep2020/day03.cpp
@@ -12,6 +12,15 @@ class Solution {
 };
 
 int main() {
+    Solution sol;
+    string s, ans;
+    bool flag;
+    // Mini program
+    std::cout << "Please enter a string: ";
+    std::cin  >> s;
+    flag = sol.repeatedSubstringPattern(s);
+    ans = flag ? "yes" : "no";
+    std::cout << "The string is a repeated substring? " << ans << std::endl;
     // Compile successful
     return 0;
 }
