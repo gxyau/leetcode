1b7675c46c1544d39b1ee9d50c2ba7575ba75807
gxyau <GuoXian.Yau@gmail.com>
2020-09-03 11:01:59 +0200

    Day 3, skeleton

---
+++ challenge/sep2020/day03.cpp
@@ -0,0 +1,17 @@
+// Day 3, Repeated Substring Pattern
+#include <iostream>
+#include <string>
+using std::string;
+
+class Solution {
+    public:
+        bool repeatedSubstringPattern(string s) {
+            bool flag = false;
+            return flag;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
