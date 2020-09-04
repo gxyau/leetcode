98d208106ff9fca78c14f67924c5e54d14e90a07
gxyau <GuoXian.Yau@gmail.com>
2020-09-04 10:19:32 +0200

    Day 3, Repeated Substring Pattern

--- challenge/sep2020/day03.cpp
+++ challenge/sep2020/day03.cpp
@@ -6,8 +6,21 @@ using std::string;
 class Solution {
     public:
         bool repeatedSubstringPattern(string s) {
-            bool flag = false;
-            return flag;
+            if (s.size() < 2) return false;
+            int n = s.size(), k;
+            string t, pattern;
+            for (int i = 1; i <= n/2; ++i) {
+                if (n % i == 0) {
+                    k = n / i;
+                    t = "";
+                    pattern = s.substr(0, i); 
+                    while(k--) {
+                        t += pattern;
+                    }
+                    if (t == s) return true;
+                }
+            }
+            return false;
         }
 };
 
