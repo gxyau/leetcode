a4ceb3594c1ce8d7a2ba32556bfa46879c3ac3c8
gxyau <GuoXian.Yau@gmail.com>
2020-09-04 10:21:50 +0200

    Day 4, skeleton

---
+++ challenge/sep2020/day04.cpp
@@ -0,0 +1,19 @@
+// Day 4, Partition Labels
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class Solution {
+    public:
+        vector<int> partitionLabels(string S) {
+            vector<int> partitions;
+            return partitions;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
