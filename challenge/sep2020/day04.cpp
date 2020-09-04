5d3faad08ca43916cbad7e49f3307f1eecd9dbb3
gxyau <GuoXian.Yau@gmail.com>
2020-09-04 10:49:02 +0200

    Day 4, added mini program

--- challenge/sep2020/day04.cpp
+++ challenge/sep2020/day04.cpp
@@ -1,5 +1,6 @@
 // Day 4, Partition Labels
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
 using std::string;
@@ -9,11 +10,27 @@ class Solution {
     public:
         vector<int> partitionLabels(string S) {
             vector<int> partitions;
-            return partitions;
+            int freq[26];
+            for (auto c : S) {
+                if (freq[c-'a']++) {
+                    partitions = {std::accumulate(partitions.begin(), partitions.end(), 0)};
+                }
+            }
+            return {};// partitions;
         }
 };
 
 int main() {
+    Solution sol;
+    string S;
+    vector<int> partitions;
+    // Mini program
+    std::cout << "Please input a string: ";
+    std::cin  >> S;
+    partitions = sol.partitionLabels(S);
+    std::cout << "Partitions: ";
+    for (int x : partitions) std::cout << x << " ";
+    std::cout << std::endl;
     // Compile successful
     return 0;
 }
