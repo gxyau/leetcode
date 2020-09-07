fd2b8ff19bea71d1b32923f38794528f64804276
gxyau <GuoXian.Yau@gmail.com>
2020-09-07 13:13:02 +0200

    Day 7, Word Pattern, Single unordered map

--- challenge/sep2020/day07.cpp
+++ challenge/sep2020/day07.cpp
@@ -2,12 +2,45 @@
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 using std::string;
+using std::unordered_map;
+using std::vector;
 
 class Solution {
+    private:
+        unordered_map<string, int> M;
+        // Break str into individual words
+        vector<string> break_string(string s) {
+            vector<string> words;
+            string word = "";
+            string::iterator it = s.begin();
+            while (it != s.end()) {
+                if (std::islower(*it)) {
+                    word += *it;
+                } else if (!word.empty()) {
+                    words.push_back(word);
+                    word = "";
+                }
+                ++it;
+            }
+            if (!word.empty()) words.push_back(word);
+            return words;
+        }
     public:
         bool wordPattern(string pattern, string str) {
-            return false;
+            vector<string> words = break_string(str);
+            // If different size the the pattern does not match already
+            if (pattern.size() != words.size()) return false;
+            int n = pattern.size();
+            // Otherwise, we check for each pair
+            for (int i = 0; i < n; ++i) {
+                if (!M.count("p_" + pattern.substr(i,1))) M["p_" + pattern.substr(i,1)] = i;
+                if (!M.count(words[i])) M[words[i]] = i;
+                if (M["p_" + pattern.substr(i,1)] != M[words[i]]) return false;
+            }
+            return true;
         }
 };
 
@@ -16,9 +49,9 @@ int main() {
     string pattern, str, flag;
     // Mini program
     std::cout << "Please input pattern: ";
-    std::cin  >> pattern;
+    std::getline(std::cin, pattern);
     std::cout << "Please input words: ";
-    std::cin  >> str;
+    std::getline(std::cin, str);
     flag = sol.wordPattern(pattern, str) ? "True" : "False";
     std::cout << "String follow pattern: " << flag << std::endl;
     // Compile successful
