4cee9c30db52d4e259cc4733a9c8785ebf440a11
gxyau <GuoXian.Yau@gmail.com>
2020-09-07 11:44:42 +0200

    Day 7 skeleton

---
+++ challenge/sep2020/day07.cpp
@@ -0,0 +1,17 @@
+// Day 7, Word Pattern
+#include <cctype>
+#include <iostream>
+#include <string>
+using std::string;
+
+class Solution {
+    public:
+        bool wordPattern(string pattern, string str) {
+            return false;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
