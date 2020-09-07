77d9391e2c40ee180ce23256ebc8871ddadd1e49
gxyau <GuoXian.Yau@gmail.com>
2020-09-07 11:47:47 +0200

    Added small program

--- challenge/sep2020/day07.cpp
+++ challenge/sep2020/day07.cpp
@@ -12,6 +12,15 @@ class Solution {
 };
 
 int main() {
+    Solution sol;
+    string pattern, str, flag;
+    // Mini program
+    std::cout << "Please input pattern: ";
+    std::cin  >> pattern;
+    std::cout << "Please input words: ";
+    std::cin  >> str;
+    flag = sol.wordPattern(pattern, str) ? "True" : "False";
+    std::cout << "String follow pattern: " << flag << std::endl;
     // Compile successful
     return 0;
 }
