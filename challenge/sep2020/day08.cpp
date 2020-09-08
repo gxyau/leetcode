e39680b8a1c088cfc046e51c9a5dc76342752741
gxyau <GuoXian.Yau@gmail.com>
2020-09-08 12:10:22 +0200

    Day 8, Sum of Root to Leaf Binary Numbers, BFS

--- challenge/sep2020/day08.cpp
+++ challenge/sep2020/day08.cpp
@@ -11,10 +11,40 @@ struct TreeNode {
 };
 
 class Solution {
-public:
-    int sumRootToLeaf(TreeNode* root) {
-        return 0;
-    }
+    private:
+        int sum;
+        // BFS helper
+        void BFS(TreeNode *root, int binary) {
+            /*
+            if (!root) {
+                sum += binary;
+                return;
+            }
+            (binary <<= 1) |= root->val;
+            BFS(root->left, binary);
+            BFS(root->right, binary);
+            */
+            (binary <<= 1) |= root->val;
+            if (!root->left && !root->right) {
+                sum += binary;
+                return;
+            }
+            if (root->right) BFS(root->right, binary);
+            if (root->left)  BFS(root->left, binary);
+            return;
+        }
+    public:
+        int sumRootToLeaf(TreeNode* root) {
+            if (!root) return 0;
+            sum = 0;
+            BFS(root, 0);
+            /*
+            // We added the leaf to the sum twice (calling left, right on leaf)
+            // So need to divide by two
+            return sum/2;
+            */
+            return sum;
+        }
 };
 
 int main() {
