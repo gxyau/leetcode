1a2d25c1029362344e91910c343696abb6f085a2
gxyau <GuoXian.Yau@gmail.com>
2020-09-11 11:05:17 +0200

    Day 11, skeleton

---
+++ challenge/sep2020/day11.cpp
@@ -0,0 +1,16 @@
+// Day 11, Maximum Product Subarray
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int maxProduct(vector<int>& nums) {
+            return 0;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
