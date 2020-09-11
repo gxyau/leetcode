a7b4a51022fa3fc7eee5f88baea9be93d8ac1dd6
gxyau <GuoXian.Yau@gmail.com>
2020-09-11 11:30:08 +0200

    Added another test case, it will break the program

--- challenge/sep2020/day11.cpp
+++ challenge/sep2020/day11.cpp
@@ -11,7 +11,7 @@ class Solution {
             int n = nums.size();
             // Using DP to calculate maximum product so far
             vector<int>::iterator it = nums.begin();
-            vector<int> maximum(n+1, {numeric_limits<int>::min()});
+            vector<int> maximum(n+1, {std::numeric_limits<int>::min()});
             for (int i = 1; i <= n; ++i) {
                 maximum[i] = std::max(nums[i], maximum[i-1] * nums[i]);
             }
@@ -81,6 +81,13 @@ int main() {
     std::cout << "The maximum product is: " << max << std::endl << std::endl;
     // Test case 9
     nums = {-2,0,-1};
+    std::cout << "The vector is: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    max  = sol.maxProduct(nums);
+    std::cout << "The maximum product is: " << max << std::endl << std::endl;
+    // Test case 10
+    nums = {1,-2,-3,0,-1,2,3,3,-2,4,-1,3,-2};
     std::cout << "The vector is: ";
     for (int x : nums) std::cout << x << " ";
     std::cout << std::endl;
