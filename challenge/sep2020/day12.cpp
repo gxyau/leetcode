e7e9c5fd7c8f6fbe5020be40c5ec541ce421ad50
gxyau <GuoXian.Yau@gmail.com>
2020-09-12 12:04:45 +0200

    Day 12 skeleton

---
+++ challenge/sep2020/day12.cpp
@@ -0,0 +1,16 @@
+// Day 12, Combination Sum III
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        vector<vector<int>> combinationSum3(int k, int n) {
+            return {};
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
