2229f02ff0b60d5cf4eb4ce59bd600f82cc80c83
gxyau <GuoXian.Yau@gmail.com>
2020-09-12 12:09:24 +0200

    Added mini program

--- challenge/sep2020/day12.cpp
+++ challenge/sep2020/day12.cpp
@@ -11,6 +11,22 @@ class Solution {
 };
 
 int main() {
+    Solution sol;
+    vector<vector<int>> combinations;
+    int k, n;
+    // Mini program
+    std::cout << "Please enter the number of partition(s), k (A number from 1-9): ";
+    std::cin  >> k;
+    std::cout << "Please enter the sum, n: ";
+    std::cin  >> n;
+    combinations = sol.combinationSum3(k,n);
+    std::cout << "The combinations are:\n";
+    for (vector<int> comb : combinations) {
+        for (int x : comb) {
+            std::cout << x << " ";
+        }
+        std::cout << std::endl;
+    }
     // Compile successful
     return 0;
 }
