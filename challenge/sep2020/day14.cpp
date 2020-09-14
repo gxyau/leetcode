23fbcbeacd29dc0b6cdfbeb4e80c373ed6a853cf
gxyau <GuoXian.Yau@gmail.com>
2020-09-14 09:52:27 +0200

    Added basic test cases

--- challenge/sep2020/day14.cpp
+++ challenge/sep2020/day14.cpp
@@ -6,11 +6,40 @@ using std::vector;
 class Solution {
     public:
         int rob(vector<int>& nums) {
-
+            if (nums.empty()) return 0;
+            int sum = 0;
+            return sum;
         }
 };
 
 int main() {
+    Solution sol;
+    vector<int> nums;
+    int sum;
+    // Test case 1
+    nums = {};
+    std::cout << "The values in the houses are:\n";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    sum = sol.rob(nums);
+    std::cout << "The maximum is: " << sum << std::endl;
+    std::cout << std::endl;
+    // Test case 2
+    nums = {1,2,3,1};
+    std::cout << "The values in the houses are:\n";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    sum = sol.rob(nums);
+    std::cout << "The maximum is: " << sum << std::endl;
+    std::cout << std::endl;
+    // Test case 3
+    nums = {2,7,9,3,1};
+    std::cout << "The values in the houses are:\n";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    sum = sol.rob(nums);
+    std::cout << "The maximum is: " << sum << std::endl;
+    std::cout << std::endl;
     // Compile successful
     return 0;
 }
