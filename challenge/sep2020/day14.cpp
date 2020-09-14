aada3f5b8ddd1428afb871cf9f6b5a7413be4b00
gxyau <GuoXian.Yau@gmail.com>
2020-09-14 09:48:07 +0200

    Day 14, skeleton

---
+++ challenge/sep2020/day14.cpp
@@ -0,0 +1,16 @@
+// Day 14, House Robber
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int rob(vector<int>& nums) {
+
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
