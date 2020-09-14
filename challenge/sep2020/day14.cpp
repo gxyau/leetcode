97eeb6015827ebad94d3686581734a37060bf369
gxyau <GuoXian.Yau@gmail.com>
2020-09-14 09:54:28 +0200

    Added more random test cases

--- challenge/sep2020/day14.cpp
+++ challenge/sep2020/day14.cpp
@@ -40,6 +40,22 @@ int main() {
     sum = sol.rob(nums);
     std::cout << "The maximum is: " << sum << std::endl;
     std::cout << std::endl;
+    // Test case 4
+    nums = {1,2,1,1,3,4,5};
+    std::cout << "The values in the houses are:\n";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    sum = sol.rob(nums);
+    std::cout << "The maximum is: " << sum << std::endl;
+    std::cout << std::endl;
+    // Test case 1
+    nums = {2,1,2,4,3,7,3,5,6,8};
+    std::cout << "The values in the houses are:\n";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    sum = sol.rob(nums);
+    std::cout << "The maximum is: " << sum << std::endl;
+    std::cout << std::endl;
     // Compile successful
     return 0;
 }
