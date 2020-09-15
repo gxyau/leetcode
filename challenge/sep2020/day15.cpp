2d6bd175e40522736a351fd542ddcc243d60fdde
gxyau <GuoXian.Yau@gmail.com>
2020-09-15 10:55:43 +0200

    Day 15, skeleton

---
+++ challenge/sep2020/day15.cpp
@@ -0,0 +1,17 @@
+// Length of Last Word
+#include <iostream>
+#include <string>
+using std::string;
+
+class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+            int length = 0;
+            return length;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
