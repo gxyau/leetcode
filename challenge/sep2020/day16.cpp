c2c3021840c55acb4d3cc197869115a01f5c8044
gxyau <GuoXian.Yau@gmail.com>
2020-09-16 19:53:07 +0200

    Added test cases

--- challenge/sep2020/day16.cpp
+++ challenge/sep2020/day16.cpp
@@ -6,12 +6,57 @@ using std::vector;
 class Solution {
     public:
         int findMaximumXOR(vector<int>& nums) {
-            int xor = 0;
-            return xor;
+            int XOR = 0;
+            return XOR;
         }
 };
 
 int main() {
+    Solution sol;
+    vector<int> nums;
+    int res;
+    // Test case 1
+    nums = {};
+    std::cout << "The vector consist of: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    res  = sol.findMaximumXOR(nums);
+    std::cout << "Maximum XOR is: " << res << std::endl;
+    // Test case 2
+    nums = {1};
+    std::cout << "The vector consist of: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    res  = sol.findMaximumXOR(nums);
+    std::cout << "Maximum XOR is: " << res << std::endl;
+    // Test case 3
+    nums = {2};
+    std::cout << "The vector consist of: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    res  = sol.findMaximumXOR(nums);
+    std::cout << "Maximum XOR is: " << res << std::endl;
+    // Test case 4
+    nums = {1,1};
+    std::cout << "The vector consist of: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    res  = sol.findMaximumXOR(nums);
+    std::cout << "Maximum XOR is: " << res << std::endl;
+    // Test case 5
+    nums = {1,2};
+    std::cout << "The vector consist of: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    res  = sol.findMaximumXOR(nums);
+    std::cout << "Maximum XOR is: " << res << std::endl;
+    // Test case 6
+    nums = {3,10,5,25,2,8};
+    std::cout << "The vector consist of: ";
+    for (int x : nums) std::cout << x << " ";
+    std::cout << std::endl;
+    res  = sol.findMaximumXOR(nums);
+    std::cout << "Maximum XOR is: " << res << std::endl;
     // Compile successful
     return 0;
 }
