012d7a83a08c1663ec247dcc5bb14f1a5b556c97
gxyau <GuoXian.Yau@gmail.com>
2020-09-16 19:47:37 +0200

    Day 16 skeleton

---
+++ challenge/sep2020/day16.cpp
@@ -0,0 +1,17 @@
+// Maximum XOR of Two Numbers in an Array
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        int findMaximumXOR(vector<int>& nums) {
+            int xor = 0;
+            return xor;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
