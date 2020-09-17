bfa83c818387fdbc6fd521dc4026c5910ff7b7b8
gxyau <GuoXian.Yau@gmail.com>
2020-09-17 09:30:41 +0200

    Day 17, skeleton

---
+++ challenge/sep2020/day17.cpp
@@ -0,0 +1,18 @@
+// Day 17, Robot Bounded In Circle
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class Solution {
+    public:
+        bool isRobotBounded(string instructions) {
+            return true;
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
