4af4b8715d087bedfa6153480002161f42aa4874
gxyau <GuoXian.Yau@gmail.com>
2020-09-19 10:20:09 +0200

    Day 19, skeleton

---
+++ challenge/sep2020/day19.cpp
@@ -0,0 +1,16 @@
+// Day 19, Sequential Digits
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+    public:
+        vector<int> sequentialDigits(int low, int high) {
+            return {};
+        }
+};
+
+int main() {
+    // Compile successful
+    return 0;
+}
