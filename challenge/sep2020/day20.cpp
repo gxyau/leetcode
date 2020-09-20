3cb4e4598fe989c18176e558f7ca498da605548f
gxyau <GuoXian.Yau@gmail.com>
2020-09-20 23:09:25 +0200

    Day 20, Unique Paths III, DFS + back tracking

--- challenge/sep2020/day20.cpp
+++ challenge/sep2020/day20.cpp
@@ -4,12 +4,50 @@
 using std::vector;
 
 class Solution {
+    private:
+        int total;
+        // DFS + Backtracking
+        void DFS(vector<vector<int>> &grid, int posx, int posy, int non_obs) {
+            // Constants
+            const int m = grid.size(), n = grid[0].size(), original = grid[posx][posy];
+            // DFS
+            grid[posx][posy] = -1; // Indicates this cell has been visited or blocked
+            if (original == 2 && non_obs == 0) {
+                ++total;
+            } else if (original == 0) {
+                if (posx > 0) DFS(grid, posx-1, posy, non_obs-1);
+                if (posx < m-1) DFS(grid, posx+1, posy, non_obs-1);
+                if (posy > 0) DFS(grid, posx, posy-1, non_obs-1);
+                if (posy < n-1) DFS(grid, posx, posy+1, non_obs-1);
+            } else if (original == 1) {
+                if (posx > 0) DFS(grid, posx-1, posy, non_obs);
+                if (posx < m-1) DFS(grid, posx+1, posy, non_obs);
+                if (posy > 0) DFS(grid, posx, posy-1, non_obs);
+                if (posy < n-1) DFS(grid, posx, posy+1, non_obs);
+            }
+            // Return to original value
+            grid[posx][posy] = original;
+            return;
+        }
     public:
         int uniquePathsIII(vector<vector<int>>& grid) {
             if (grid.empty() or grid[0].empty()) return 0;
-            int m = grid.size(), n = grid[0].size();
-            vector<vector<int>> DP(m+1,vector<int>(n+1,{0}));
-            return DP[m][n];
+            const int m = grid.size(), n = grid[0].size();
+            int startx, starty, non_obs = 0;
+            // Find starting point
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == 1) {
+                        startx = i;
+                        starty = j;
+                    } else if (grid[i][j] == 0) {
+                        ++non_obs;
+                    }
+                }
+            }
+            total = 0;
+            DFS(grid, startx, starty, non_obs);
+            return total;
         }
 };
 
